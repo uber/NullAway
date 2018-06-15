c9c03741e3c7eed1f12b36961cf6181014b31ef7
Subarno Banerjee <subarno@uber.com>
2018-06-15 16:35:12 -0700

    junit integration

--- jar-infer/src/test/resources/com/uber/nullaway/jarinfer/testdata/Toys.java
+++ jar-infer/src/test/resources/com/uber/nullaway/jarinfer/testdata/Toys.java
@@ -24,42 +24,12 @@ class Foo {
   }
 }
 
-class Bar {
-  private String bar;
-  public int b;
-
-  public Bar(String str) {
-    if (str == null) str = "bar";
-    this.bar = str;
-    this.b = bar.length();
-  }
-
-  public int run(String str) {
-    if (str != null) {
-      return str.length();
-    }
-    return bar.length();
-  }
-}
-
-public class Main {
-  public static void test(String s, Foo f, Bar b) {
-    b.b = 5;
-    if (s.length() >= 5) {
-      Foo f1 = new Foo(s);
-      f1.run(s);
-    } else {
-      f.run(s);
-    }
-    b.run(s);
-  }
-
+public class Toys {
   public static void main(String arg[]) throws java.io.IOException {
     String s = new String("test string...");
     Foo f = new Foo("try");
-    Bar b = new Bar(null);
     try {
-      test(s, f, b);
+      f.test(s, "string test...");
     } catch (Error e) {
       System.out.println(e.getMessage());
     }
