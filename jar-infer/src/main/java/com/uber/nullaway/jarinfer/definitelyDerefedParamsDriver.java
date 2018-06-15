017271cd2c77acea43a37958f0cd0968ea43753e
Subarno Banerjee <subarno@uber.com>
2018-06-14 17:08:11 -0700

    cleanup

--- jar-infer/src/main/java/com/uber/nullaway/jarinfer/definitelyDerefedParamsDriver.java
+++ jar-infer/src/main/java/com/uber/nullaway/jarinfer/definitelyDerefedParamsDriver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.uber.nullaway.jarinfer;
 
 import com.ibm.wala.cfg.ControlFlowGraph;
@@ -6,7 +21,6 @@ import com.ibm.wala.ipa.callgraph.AnalysisCache;
 import com.ibm.wala.ipa.callgraph.AnalysisCacheImpl;
 import com.ibm.wala.ipa.callgraph.AnalysisOptions;
 import com.ibm.wala.ipa.callgraph.AnalysisScope;
-import com.ibm.wala.ipa.callgraph.CallGraphBuilderCancelException;
 import com.ibm.wala.ipa.callgraph.impl.Everywhere;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
@@ -46,8 +60,7 @@ public class DefinitelyDerefedParamsDriver {
    * @throws IllegalArgumentException
    */
   public static void main(String[] args)
-      throws IOException, ClassHierarchyException, IllegalArgumentException,
-          CallGraphBuilderCancelException {
+      throws IOException, ClassHierarchyException, IllegalArgumentException {
     long start = System.currentTimeMillis();
     Properties p = CommandLine.parse(args);
     String scopeFile = p.getProperty("scopeFile");
