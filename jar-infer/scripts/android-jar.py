f7ce9f2c8060d30ebcfe204ac1d2c88e00bd3b44
Subarno <subarno@umich.edu>
2018-08-07 15:41:31 -0700

    Handler for separate Android models jar (#206)
    - Instead of adding JarInfer models to the stubs android.jar, we create a new Android-library-models jar.
    - The NullAway handler initially loads the JarInfer models for the Android libraries into the Annotation Cache, and all subsequent invocations of Android class methods hit the Annotation Cache.

--- jar-infer/scripts/android-jar.py
+++ jar-infer/scripts/android-jar.py
@@ -4,19 +4,23 @@ from optparse import OptionParser
 class2jar = dict()
 jars_to_process = set()
 
-parser = OptionParser()
-parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="set verbosity")
-parser.add_option("-c", "--config", dest="config", default=os.path.dirname(sys.argv[0])+"/android-jar.conf", help="configuration file path")
-(options, args) = parser.parse_args()
+opt_parser = OptionParser()
+opt_parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="set verbosity")
+opt_parser.add_option("-c", "--config", dest="config", default=os.path.dirname(sys.argv[0])+"/android-jar.conf", help="configuration file path")
+(options, args) = opt_parser.parse_args()
 if not os.path.isfile(options.config):
   sys.exit('Error! No configuration file at: '+options.config)
-configParser = ConfigParser.RawConfigParser()   
-configParser.read(options.config)
-ajars_dir = configParser.get('android-paths', 'aosp-out-dir')
-stubsjar = configParser.get('android-paths', 'android-jar-path')
-nullaway_dir = configParser.get('jar-infer-paths', 'nullaway-dir')
-jarinfer = configParser.get('jar-infer-paths', 'jar-infer-path')
-astubx_file = configParser.get('jar-infer-paths', 'astubx-path')
+config_parser = ConfigParser.SafeConfigParser()
+config_parser.read(options.config)
+ajars_dir = config_parser.get('android-paths', 'aosp-out-dir')
+stubs_jar = config_parser.get('android-paths', 'android-stubs-jar')
+jarinfer = config_parser.get('jar-infer-paths', 'jar-infer-path')
+out_jar = config_parser.get('android-model', 'android-model-jar')
+wrk_dir = config_parser.get('android-model', 'wrk-dir')
+astubx_file = config_parser.get('android-model', 'astubx-path')
+package_name = config_parser.get('android-model', 'package-name')
+class_name = config_parser.get('android-model', 'class-name')
+dummy_code = config_parser.get('android-model', 'dummy-code')
 
 ### Finding android libraries ###
 print "> Finding android libraries..."
@@ -31,7 +35,7 @@ for ajar in ajars:
         print "[Warn] Same class in multiple jars : " + ajar_class + " in " + class2jar[ajar_class] + " , " + ajar
     else:
       class2jar[ajar_class] = ajar
-cmd = "jar tvf " + stubsjar + " | grep \"\.class$\" | awk '{print $8}'"
+cmd = "jar tvf " + stubs_jar + " | grep \"\.class$\" | awk '{print $8}'"
 ajar_classes = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).stdout.read().splitlines()
 found = 0
 for ajar_class in ajar_classes:
@@ -46,13 +50,16 @@ print "Found " + str(found) + " / " + str(len(ajar_classes)) + " in " + str(len(
 
 ### Running jarinfer ###
 print "> Running jarinfer on android jars..."
-cmd = "java -jar " + jarinfer + " -i " + ",".join(list(jars_to_process)) + " -o " + nullaway_dir + "/" + astubx_file
+cmd = "mkdir -p "+wrk_dir+" && java -jar " + jarinfer + " -i " + ",".join(list(jars_to_process)) + " -o " + wrk_dir + "/" + astubx_file
 if options.verbose:
   cmd += " -dv"
 subprocess.call(cmd, shell=True)
 
 ### Writing models ###
-print "> Writing jarinfer models to android jar..."
-cmd = "cd " + nullaway_dir + " && jar uf " + stubsjar + " " + astubx_file
+print "> Writing jarinfer models for android jar..."
+prefix = package_name.replace(".","/") + "/"
+cmd = "cd "+wrk_dir+" && mkdir -p "+prefix+" && echo \""+dummy_code+"\" > "+prefix+class_name+".java && javac "+prefix+class_name+".java && jar cf "+out_jar+" "+prefix+class_name+".class "+astubx_file+" && jar tvf "+out_jar
 subprocess.call(cmd, shell=True)
-print "> Done. Annotated android jar: " + stubsjar
+print "> Done! Android model jar: " + os.path.expandvars(out_jar)
+
+### Uploading models ###
