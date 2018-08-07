250aba0fb95fc7ddbd32532bce62e4dd0e294d65
Subarno Banerjee <subarno@uber.com>
2018-08-07 14:11:51 -0700

    cleanup

--- jar-infer/scripts/android-jar.py
+++ jar-infer/scripts/android-jar.py
@@ -4,23 +4,23 @@ from optparse import OptionParser
 class2jar = dict()
 jars_to_process = set()
 
-parser = OptionParser()
-parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="set verbosity")
-parser.add_option("-c", "--config", dest="config", default=os.path.dirname(sys.argv[0])+"/android-jar.conf", help="configuration file path")
-(options, args) = parser.parse_args()
+opt_parser = OptionParser()
+opt_parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="set verbosity")
+opt_parser.add_option("-c", "--config", dest="config", default=os.path.dirname(sys.argv[0])+"/android-jar.conf", help="configuration file path")
+(options, args) = opt_parser.parse_args()
 if not os.path.isfile(options.config):
   sys.exit('Error! No configuration file at: '+options.config)
-configParser = ConfigParser.SafeConfigParser()
-configParser.read(options.config)
-ajars_dir = configParser.get('android-paths', 'aosp-out-dir')
-stubs_jar = configParser.get('android-paths', 'android-stubs-jar')
-jarinfer = configParser.get('jar-infer-paths', 'jar-infer-path')
-out_jar = configParser.get('android-model', 'android-model-jar')
-wrk_dir = configParser.get('android-model', 'wrk-dir')
-astubx_file = configParser.get('android-model', 'astubx-path')
-package_name = configParser.get('android-model', 'package-name')
-class_name = configParser.get('android-model', 'class-name')
-dummy_code = configParser.get('android-model', 'dummy-code')
+config_parser = ConfigParser.SafeConfigParser()
+config_parser.read(options.config)
+ajars_dir = config_parser.get('android-paths', 'aosp-out-dir')
+stubs_jar = config_parser.get('android-paths', 'android-stubs-jar')
+jarinfer = config_parser.get('jar-infer-paths', 'jar-infer-path')
+out_jar = config_parser.get('android-model', 'android-model-jar')
+wrk_dir = config_parser.get('android-model', 'wrk-dir')
+astubx_file = config_parser.get('android-model', 'astubx-path')
+package_name = config_parser.get('android-model', 'package-name')
+class_name = config_parser.get('android-model', 'class-name')
+dummy_code = config_parser.get('android-model', 'dummy-code')
 
 ### Finding android libraries ###
 print "> Finding android libraries..."
@@ -60,6 +60,6 @@ print "> Writing jarinfer models for android jar..."
 prefix = package_name.replace(".","/") + "/"
 cmd = "cd "+wrk_dir+" && mkdir -p "+prefix+" && echo \""+dummy_code+"\" > "+prefix+class_name+".java && javac "+prefix+class_name+".java && jar cf "+out_jar+" "+prefix+class_name+".class "+astubx_file+" && jar tvf "+out_jar
 subprocess.call(cmd, shell=True)
-print "> Done! Android model jar: " + out_jar
+print "> Done! Android model jar: " + os.path.expandvars(out_jar)
 
 ### Uploading models ###
