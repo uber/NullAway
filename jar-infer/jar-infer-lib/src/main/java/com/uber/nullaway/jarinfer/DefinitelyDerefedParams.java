138e323e3cec7de9a8eb560815e55639d2990f4e
Lazaro Clapp <lazaro@uber.com>
2019-06-12 14:30:31 -0700

    [TEST] [DO NOT MERGE] Checking Travis build.

--- jar-infer/jar-infer-lib/src/main/java/com/uber/nullaway/jarinfer/DefinitelyDerefedParams.java
+++ jar-infer/jar-infer-lib/src/main/java/com/uber/nullaway/jarinfer/DefinitelyDerefedParams.java
@@ -46,6 +46,8 @@ import java.util.Set;
 public class DefinitelyDerefedParams {
   private static final boolean DEBUG = false;
 
+  private static final String rebuildMe = "rebuild me!";
+
   private static void LOG(boolean cond, String tag, String msg) {
     if (cond) System.out.println("[JI " + tag + "] " + msg);
   }
