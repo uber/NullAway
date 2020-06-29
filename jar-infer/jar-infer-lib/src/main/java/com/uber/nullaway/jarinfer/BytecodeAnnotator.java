3e2233b75507c32e81cf0ba9e8610bcd3f8acf1e
Lázaro Clapp <lazaro@uber.com>
2020-06-29 17:25:45 -0400

    Prevent memory copy of list of Jar/Aar file entries. (#404)
    See #403 discussion, credit for this optimization goes to @Stephan202.
    
    Note the comment edit for the AAR case. ZipFile is just surprisingly
    hostile to copy-free for-each iteration.

--- jar-infer/jar-infer-lib/src/main/java/com/uber/nullaway/jarinfer/BytecodeAnnotator.java
+++ jar-infer/jar-infer-lib/src/main/java/com/uber/nullaway/jarinfer/BytecodeAnnotator.java
@@ -15,7 +15,6 @@
  */
 package com.uber.nullaway.jarinfer;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import java.io.BufferedReader;
@@ -24,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
@@ -280,7 +280,7 @@ public final class BytecodeAnnotator {
     // Reference: https://bugs.openjdk.java.net/browse/JDK-8215788
     // Note: we can't just put the code below inside stream().forach(), because it can throw
     // IOException.
-    for (JarEntry jarEntry : inputJar.stream().collect(ImmutableList.toImmutableList())) {
+    for (JarEntry jarEntry : (Iterable<JarEntry>) inputJar.stream()::iterator) {
       InputStream is = inputJar.getInputStream(jarEntry);
       copyAndAnnotateJarEntry(
           jarEntry,
@@ -317,10 +317,13 @@ public final class BytecodeAnnotator {
     LOG(debug, "DEBUG", "nullableReturns: " + nullableReturns);
     LOG(debug, "DEBUG", "nonnullParams: " + nonnullParams);
     // Error Prone doesn't like usages of the old Java Enumerator APIs. ZipFile does not implement
-    // Iterable, and likely
-    // never will (see  https://bugs.openjdk.java.net/browse/JDK-6581715). So this seems like the
+    // Iterable, and likely never will (see  https://bugs.openjdk.java.net/browse/JDK-6581715).
+    // Additionally, inputZip.stream() returns a Stream<? extends ZipEntry>, and a for-each loop
+    // has trouble handling the corresponding ::iterator  method reference. So this seems like the
     // best remaining way:
-    for (ZipEntry zipEntry : inputZip.stream().collect(ImmutableList.toImmutableList())) {
+    Iterator<? extends ZipEntry> zipIterator = inputZip.stream().iterator();
+    while (zipIterator.hasNext()) {
+      ZipEntry zipEntry = zipIterator.next();
       InputStream is = inputZip.getInputStream(zipEntry);
       zipOS.putNextEntry(new ZipEntry(zipEntry.getName()));
       if (zipEntry.getName().equals("classes.jar")) {
