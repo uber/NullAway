63cd5095b9cc021c1d5b1b7db9a99d4353c6b2d5
nimakarimipour <karimipour.nima@gmail.com>
2020-09-06 14:02:43 -0500

    update

---
+++ nullaway/src/main/java/com/uber/nullaway/qual/Contract.java
@@ -0,0 +1,13 @@
+package com.uber.nullaway.qual;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({FIELD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Contract {
+  String value();
+}
