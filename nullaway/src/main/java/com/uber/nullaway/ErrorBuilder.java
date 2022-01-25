f4f1a844adc431ed78953e57580aef1592f70456
nimakarimipour <karimipour.nima@gmail.com>
2022-01-25 12:58:36 -0800

    Created a facade class for Fix Serialization Package.

--- nullaway/src/main/java/com/uber/nullaway/ErrorBuilder.java
+++ nullaway/src/main/java/com/uber/nullaway/ErrorBuilder.java
@@ -32,7 +32,6 @@ import static com.uber.nullaway.NullAway.OPTIONAL_CHECK_NAME;
 import static com.uber.nullaway.NullAway.getTreesInstance;
 
 import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
@@ -49,18 +48,11 @@ import com.sun.source.tree.ModifiersTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
-import com.sun.source.util.Trees;
 import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.util.DiagnosticSource;
 import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
-import com.uber.nullaway.fixserialization.FixSerializationConfig;
-import com.uber.nullaway.fixserialization.Serializer;
-import com.uber.nullaway.fixserialization.location.AbstractFixLocation;
-import com.uber.nullaway.fixserialization.location.FixLocation;
-import com.uber.nullaway.fixserialization.out.ErrorInfo;
-import com.uber.nullaway.fixserialization.out.SuggestedFixInfo;
+import com.uber.nullaway.fixserialization.SerializationHandler;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -145,9 +137,9 @@ public class ErrorBuilder {
 
     if (config.serializationIsActive()) {
       if (nonNullTarget != null) {
-        serializeFixSuggestion(state, nonNullTarget, errorMessage);
+        SerializationHandler.serializeFixSuggestion(config, state, nonNullTarget, errorMessage);
       }
-      serializeReportingError(state, errorMessage);
+      SerializationHandler.serializeReportingError(config, state, errorMessage);
     }
 
     // #letbuildersbuild
@@ -375,7 +367,9 @@ public class ErrorBuilder {
     if (config.serializationIsActive()) {
       // For now, we serialize each fix suggestion separately and measure their effectiveness
       // separately
-      nonNullFields.forEach(symbol -> serializeFixSuggestion(state, symbol, errorMessage));
+      nonNullFields.forEach(
+          symbol ->
+              SerializationHandler.serializeFixSuggestion(config, state, symbol, errorMessage));
     }
   }
 
@@ -491,76 +485,5 @@ public class ErrorBuilder {
               state,
               symbol));
     }
-  }
-
-  /**
-   * Serializes the suggested type change of an element in the source code that can resolve the
-   * error. We do not want suggested fix changes to override explicit annotations in the code,
-   * therefore, if the target element has an explicit {@code @Nonnull} annotation, no type change is
-   * suggested.
-   *
-   * @param state Visitor state.
-   * @param target Target element to alternate it's type.
-   * @param errorMessage Error caused by the target.
-   */
-  public void serializeFixSuggestion(VisitorState state, Symbol target, ErrorMessage errorMessage) {
-    FixSerializationConfig serializationConfig = config.getSerializationConfig();
-    if (!serializationConfig.suggestEnabled) {
-      return;
-    }
-    // Skip if the element has an explicit @Nonnull annotation.
-    if (Nullness.hasNonNullAnnotation(target, config)) {
-      return;
-    }
-    Trees trees = Trees.instance(JavacProcessingEnvironment.instance(state.context));
-    // Skip if the element is received as bytecode.
-    if (trees.getPath(target) == null) {
-      return;
-    }
-    FixLocation location = AbstractFixLocation.createFixLocationFromSymbol(target);
-    SuggestedFixInfo suggestedFixInfo = buildFixMetadata(state.getPath(), errorMessage, location);
-    Serializer serializer = serializationConfig.getSerializer();
-    Preconditions.checkNotNull(
-        serializer, "Serializer shouldn't be null at this point, error in configuration setting!");
-    serializer.serializeSuggestedFixInfo(suggestedFixInfo, serializationConfig.suggestEnclosing);
-  }
-
-  /**
-   * Serializes the reporting error.
-   *
-   * @param state Visitor state.
-   * @param errorMessage Error caused by the target.
-   */
-  public void serializeReportingError(VisitorState state, ErrorMessage errorMessage) {
-    Serializer serializer = config.getSerializationConfig().getSerializer();
-    Preconditions.checkNotNull(
-        serializer, "Serializer shouldn't be null at this point, error in configuration setting!");
-    serializer.serializeErrorInfo(new ErrorInfo(state.getPath(), errorMessage));
-  }
-
-  /** Builds the {@link SuggestedFixInfo} instance based on the {@link ErrorMessage} type. */
-  protected SuggestedFixInfo buildFixMetadata(
-      TreePath path, ErrorMessage errorMessage, FixLocation location) {
-    SuggestedFixInfo suggestedFixInfo;
-    switch (errorMessage.getMessageType()) {
-      case RETURN_NULLABLE:
-      case WRONG_OVERRIDE_RETURN:
-      case WRONG_OVERRIDE_PARAM:
-      case PASS_NULLABLE:
-      case FIELD_NO_INIT:
-      case ASSIGN_FIELD_NULLABLE:
-      case METHOD_NO_INIT:
-        suggestedFixInfo =
-            new SuggestedFixInfo(
-                path,
-                location,
-                errorMessage,
-                config.getSerializationConfig().annotationConfig.getNullable());
-        break;
-      default:
-        throw new IllegalStateException(
-            "Cannot suggest a type to resolve error with type: " + errorMessage.getMessageType());
-    }
-    return suggestedFixInfo;
   }
 }
---
+++ nullaway/src/main/java/com/uber/nullaway/fixserialization/SerializationHandler.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2022 Uber Technologies, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.uber.nullaway.fixserialization;
+
+import com.google.common.base.Preconditions;
+import com.google.errorprone.VisitorState;
+import com.sun.source.util.TreePath;
+import com.sun.source.util.Trees;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.uber.nullaway.Config;
+import com.uber.nullaway.ErrorMessage;
+import com.uber.nullaway.Nullness;
+import com.uber.nullaway.fixserialization.location.AbstractFixLocation;
+import com.uber.nullaway.fixserialization.location.FixLocation;
+import com.uber.nullaway.fixserialization.out.ErrorInfo;
+import com.uber.nullaway.fixserialization.out.SuggestedFixInfo;
+
+/** A facade class to interact with fix serialization package. */
+public class SerializationHandler {
+
+  /**
+   * Serializes the suggested type change of an element in the source code that can resolve the
+   * error. We do not want suggested fix changes to override explicit annotations in the code,
+   * therefore, if the target element has an explicit {@code @Nonnull} annotation, no type change is
+   * suggested.
+   *
+   * @param config NullAway config.
+   * @param state Visitor state.
+   * @param target Target element to alternate it's type.
+   * @param errorMessage Error caused by the target.
+   */
+  public static void serializeFixSuggestion(
+      Config config, VisitorState state, Symbol target, ErrorMessage errorMessage) {
+    FixSerializationConfig serializationConfig = config.getSerializationConfig();
+    if (!serializationConfig.suggestEnabled) {
+      return;
+    }
+    // Skip if the element has an explicit @Nonnull annotation.
+    if (Nullness.hasNonNullAnnotation(target, config)) {
+      return;
+    }
+    Trees trees = Trees.instance(JavacProcessingEnvironment.instance(state.context));
+    // Skip if the element is received as bytecode.
+    if (trees.getPath(target) == null) {
+      return;
+    }
+    FixLocation location = AbstractFixLocation.createFixLocationFromSymbol(target);
+    SuggestedFixInfo suggestedFixInfo =
+        buildFixMetadata(config, state.getPath(), errorMessage, location);
+    Serializer serializer = serializationConfig.getSerializer();
+    Preconditions.checkNotNull(
+        serializer, "Serializer shouldn't be null at this point, error in configuration setting!");
+    serializer.serializeSuggestedFixInfo(suggestedFixInfo, serializationConfig.suggestEnclosing);
+  }
+
+  /**
+   * Serializes the reporting error.
+   *
+   * @param config NullAway config.
+   * @param state Visitor state.
+   * @param errorMessage Error caused by the target.
+   */
+  public static void serializeReportingError(
+      Config config, VisitorState state, ErrorMessage errorMessage) {
+    Serializer serializer = config.getSerializationConfig().getSerializer();
+    Preconditions.checkNotNull(
+        serializer, "Serializer shouldn't be null at this point, error in configuration setting!");
+    serializer.serializeErrorInfo(new ErrorInfo(state.getPath(), errorMessage));
+  }
+
+  /** Builds the {@link SuggestedFixInfo} instance based on the {@link ErrorMessage} type. */
+  private static SuggestedFixInfo buildFixMetadata(
+      Config config, TreePath path, ErrorMessage errorMessage, FixLocation location) {
+    SuggestedFixInfo suggestedFixInfo;
+    switch (errorMessage.getMessageType()) {
+      case RETURN_NULLABLE:
+      case WRONG_OVERRIDE_RETURN:
+      case WRONG_OVERRIDE_PARAM:
+      case PASS_NULLABLE:
+      case FIELD_NO_INIT:
+      case ASSIGN_FIELD_NULLABLE:
+      case METHOD_NO_INIT:
+        suggestedFixInfo =
+            new SuggestedFixInfo(
+                path,
+                location,
+                errorMessage,
+                config.getSerializationConfig().annotationConfig.getNullable());
+        break;
+      default:
+        throw new IllegalStateException(
+            "Cannot suggest a type to resolve error with type: " + errorMessage.getMessageType());
+    }
+    return suggestedFixInfo;
+  }
+}
