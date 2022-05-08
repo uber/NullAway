6d390d0bcfef8e8d59a613a27adaecd146254ef7
nimakarimipour <karimipour.nima@gmail.com>
2022-05-07 20:00:39 -0500

    refactor

--- nullaway/src/main/java/com/uber/nullaway/fixserialization/out/ClassAndMethodInfo.java
+++ nullaway/src/main/java/com/uber/nullaway/fixserialization/out/ClassAndMethodInfo.java
@@ -25,7 +25,6 @@ package com.uber.nullaway.fixserialization.out;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Symbol;
 import javax.annotation.Nullable;
@@ -49,43 +48,21 @@ public class ClassAndMethodInfo {
 
   /** Finds the class and method where the error is reported according to {@code path}. */
   public void findValues() {
-    method = ASTHelpers.findEnclosingNode(path, MethodTree.class);
+    method =
+        path.getLeaf() instanceof MethodTree
+            ? (MethodTree) path.getLeaf()
+            : ASTHelpers.findEnclosingNode(path, MethodTree.class);
     clazz =
-        path.getLeaf().getKind().equals(Tree.Kind.CLASS)
+        path.getLeaf() instanceof ClassTree
             ? (ClassTree) path.getLeaf()
             : ASTHelpers.findEnclosingNode(path, ClassTree.class);
-    if (clazz == null && path.getLeaf() instanceof ClassTree) {
-      clazz = (ClassTree) path.getLeaf();
-    }
-    if (path.getLeaf() instanceof MethodTree) {
-      method = (MethodTree) path.getLeaf();
-    }
     if (clazz != null && method != null) {
       Symbol.ClassSymbol classSymbol = ASTHelpers.getSymbol(clazz);
       Symbol.MethodSymbol methodSymbol = ASTHelpers.getSymbol(method);
-      if (methodIsEnclosingClass(classSymbol, methodSymbol)) {
+      if (!methodSymbol.isEnclosedBy(classSymbol)) {
         method = null;
       }
     }
-  }
-
-  /**
-   * Returns true, if the method is enclosing the class.
-   *
-   * @param clazz class symbol.
-   * @param method method symbol.
-   * @return true, if method is enclosing class.
-   */
-  private static boolean methodIsEnclosingClass(
-      Symbol.ClassSymbol clazz, Symbol.MethodSymbol method) {
-    Symbol enclosingSymbol = method.getEnclosingElement();
-    while (enclosingSymbol != null) {
-      if (clazz.equals(enclosingSymbol)) {
-        return false;
-      }
-      enclosingSymbol = enclosingSymbol.getEnclosingElement();
-    }
-    return true;
   }
 
   @Nullable
