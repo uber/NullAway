5b17e25841f82cddd32c42adeb1df1d7342803e6
Manu Sridharan <msridhar@gmail.com>
2022-06-29 14:24:13 -0700

    tweak

--- jmh/src/main/java/com/uber/nullaway/jmh/DataFlowMicroBenchmarkCompiler.java
+++ jmh/src/main/java/com/uber/nullaway/jmh/DataFlowMicroBenchmarkCompiler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class DataFlowMicroBenchmarkCompiler {
 
@@ -30,7 +31,8 @@ public class DataFlowMicroBenchmarkCompiler {
 
   private static String readFromInputStream(InputStream inputStream) throws IOException {
     StringBuilder result = new StringBuilder();
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+    try (BufferedReader br =
+        new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
       String line;
       while ((line = br.readLine()) != null) {
         result.append(line).append("\n");
