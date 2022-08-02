5d6c1c930888d22966db59db1e0349d987ed80a1
Lazaro Clapp <lazaro@uber.com>
2022-08-02 16:25:15 -0400

    Minor fix

--- nullaway/src/main/java/com/uber/nullaway/Nullness.java
+++ nullaway/src/main/java/com/uber/nullaway/Nullness.java
@@ -158,6 +158,9 @@ public enum Nullness implements AbstractValue<Nullness> {
         || annotName.endsWith(".CheckForNull")
         // matches any of the multiple @ParametricNullness annotations used within Guava
         // (see https://github.com/google/guava/issues/6126)
+        // We check the simple name first and the package prefix second for boolean short
+        // circuiting, as Guava includes
+        // many annotations
         || (annotName.endsWith(".ParametricNullness") && annotName.startsWith("com.google.common."))
         || (config.acknowledgeAndroidRecent()
             && annotName.equals("androidx.annotation.RecentlyNullable"))
