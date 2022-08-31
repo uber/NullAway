uber/NullAway

commit 0e575ce8e0d8fa7f07404ba04c30c004348ec50b

Generalize handler APIs for argument nullability on (un-)annotated code. (#639)
In particular, this allows library models to change the nullability of method
arguments in annotated code.

This change replaces two separate an overly specific `Handler` callbacks:

- `onUnannotatedInvocationGetExplicitlyNullablePositions`
- `onUnannotatedInvocationGetNonNullPositions`

With a single more general callback:

- `onOverrideMethodInvocationParametersNullability`

which is explicitly meant to handle overriding/changing method parameter 
nullability for both annotated and unannotated code (thus solving issue #637).

This requires some refactoring (mostly simplifying logic) in the core NullAway 
class, as well as changes to the different handlers, which must now explicitly 
skip annotated code if they aren't designed to work with it (e.g. 
`RestrictiveAnnotationHandler`).

nullaway/src/main/java/com/uber/nullaway/NullAway.java
@@ -618,62 +618,70 @@ public class NullAway extends BugChecker
       @Nullable MemberReferenceTree memberReferenceTree,
       VisitorState state) {
     com.sun.tools.javac.util.List<VarSymbol> superParamSymbols = overriddenMethod.getParameters();
-    boolean unboundMemberRef =
+    final boolean unboundMemberRef =
         (memberReferenceTree != null)
             && ((JCTree.JCMemberReference) memberReferenceTree).kind.isUnbound();
-    // if we have an unbound method reference, the first parameter of the overridden method must be
-    // @NonNull, as this parameter will be used as a method receiver inside the generated lambda
-    if (unboundMemberRef) {
-      boolean isFirstParamNull = false;
-      // Two cases: for annotated code, look first at the annotation
-      if (!classAnnotationInfo.isSymbolUnannotated(overriddenMethod, config)) {
-        isFirstParamNull = Nullness.hasNullableAnnotation(superParamSymbols.get(0), config);
-      }
-      // For both annotated and unannotated code, look then at handler overrides (e.g. Library
-      // Models)
-      isFirstParamNull =
-          handler
-              .onUnannotatedInvocationGetExplicitlyNullablePositions(
-                  state.context,
-                  overriddenMethod,
-                  isFirstParamNull ? ImmutableSet.of(0) : ImmutableSet.of())
-              .contains(0);
-      if (isFirstParamNull) {
-        String message =
-            "unbound instance method reference cannot be used, as first parameter of "
-                + "functional interface method "
-                + ASTHelpers.enclosingClass(overriddenMethod)
-                + "."
-                + overriddenMethod.toString()
-                + " is @Nullable";
-        return errorBuilder.createErrorDescription(
-            new ErrorMessage(MessageTypes.WRONG_OVERRIDE_PARAM, message),
-            buildDescription(memberReferenceTree),
-            state,
-            null);
+    final boolean isOverriddenMethodAnnotated =
+        !classAnnotationInfo.isSymbolUnannotated(overriddenMethod, config);
+
+    // Get argument nullability for the overridden method:
+    Map<Integer, Nullness> overriddenMethodArgNullnessMap = new LinkedHashMap<>();
+
+    // Collect @Nullable params of overridden method iff the overridden method is in annotated code
+    // (otherwise, whether we acknowledge @Nullable in unannotated code or not depends on the
+    // -XepOpt:NullAway:AcknowledgeRestrictiveAnnotations flag and its handler).
+    if (isOverriddenMethodAnnotated) {
+      for (int i = 0; i < superParamSymbols.size(); i++) {
+        overriddenMethodArgNullnessMap.put(
+            i,
+            Nullness.paramHasNullableAnnotation(overriddenMethod, i, config)
+                ? Nullness.NULLABLE
+                : Nullness.NONNULL);
       }
     }
+
+    // Check handlers for any further/overriding nullness information
+    overriddenMethodArgNullnessMap =
+        handler.onOverrideMethodInvocationParametersNullability(
+            state.context,
+            overriddenMethod,
+            isOverriddenMethodAnnotated,
+            overriddenMethodArgNullnessMap);
+
+    // If we have an unbound method reference, the first parameter of the overridden method must be
+    // @NonNull, as this parameter will be used as a method receiver inside the generated lambda.
+    // e.g. String::length is implemented as (@NonNull s -> s.length()) when used as a
+    // SomeFunc<String> and thus incompatible with, for example, SomeFunc.apply(@Nullable T).
+    if (unboundMemberRef
+        && overriddenMethodArgNullnessMap
+            .getOrDefault(0, Nullness.NONNULL)
+            .equals(Nullness.NULLABLE)) {
+      String message =
+          "unbound instance method reference cannot be used, as first parameter of "
+              + "functional interface method "
+              + ASTHelpers.enclosingClass(overriddenMethod)
+              + "."
+              + overriddenMethod.toString()
+              + " is @Nullable";
+      return errorBuilder.createErrorDescription(
+          new ErrorMessage(MessageTypes.WRONG_OVERRIDE_PARAM, message),
+          buildDescription(memberReferenceTree),
+          state,
+          null);
+    }
+
     // for unbound member references, we need to adjust parameter indices by 1 when matching with
     // overridden method
-    int startParam = unboundMemberRef ? 1 : 0;
-    // Collect @Nullable params of overriden method
-    ImmutableSet<Integer> nullableParamsOfOverriden;
-    if (classAnnotationInfo.isSymbolUnannotated(overriddenMethod, config)) {
-      nullableParamsOfOverriden =
-          handler.onUnannotatedInvocationGetExplicitlyNullablePositions(
-              state.context, overriddenMethod, ImmutableSet.of());
-    } else {
-      ImmutableSet.Builder<Integer> builder = ImmutableSet.builder();
-      for (int i = startParam; i < superParamSymbols.size(); i++) {
-        // we need to call paramHasNullableAnnotation here since overriddenMethod may be defined
-        // in a class file
-        if (Nullness.paramHasNullableAnnotation(overriddenMethod, i, config)) {
-          builder.add(i);
-        }
+    final int startParam = unboundMemberRef ? 1 : 0;
+
+    for (int i = 0; i < superParamSymbols.size(); i++) {
+      if (overriddenMethodArgNullnessMap
+          .getOrDefault(i, Nullness.NONNULL)
+          .equals(Nullness.NONNULL)) {
+        // No need to check, unless the argument of the overridden method is effectively @Nullable,
+        // in which case it can't be overridding a @NonNull arg.
+        continue;
       }
-      nullableParamsOfOverriden = builder.build();
-    }
-    for (int i : nullableParamsOfOverriden) {
       int methodParamInd = i - startParam;
       VarSymbol paramSymbol = overridingParamSymbols.get(methodParamInd);
       // in the case where we have a parameter of a lambda expression, we do
@@ -1425,12 +1433,6 @@ public class NullAway extends BugChecker
       VisitorState state,
       Symbol.MethodSymbol methodSymbol,
       List<? extends ExpressionTree> actualParams) {
-    ImmutableSet<Integer> nonNullPositions = null;
-    if (classAnnotationInfo.isSymbolUnannotated(methodSymbol, config)) {
-      nonNullPositions =
-          handler.onUnannotatedInvocationGetNonNullPositions(
-              this, state, methodSymbol, actualParams, ImmutableSet.of());
-    }
     List<VarSymbol> formalParams = methodSymbol.getParameters();
 
     if (formalParams.size() != actualParams.size()
@@ -1444,8 +1446,11 @@ public class NullAway extends BugChecker
       return Description.NO_MATCH;
     }
 
-    if (nonNullPositions == null) {
-      ImmutableSet.Builder<Integer> builder = ImmutableSet.builder();
+    final boolean isMethodAnnotated =
+        !classAnnotationInfo.isSymbolUnannotated(methodSymbol, config);
+    Map<Integer, Nullness> argumentPositionNullness = new LinkedHashMap<>();
+
+    if (isMethodAnnotated) {
       // compute which arguments are @NonNull
       for (int i = 0; i < formalParams.size(); i++) {
         VarSymbol param = formalParams.get(i);
@@ -1454,27 +1459,39 @@ public class NullAway extends BugChecker
           if (unboxingCheck != Description.NO_MATCH) {
             return unboxingCheck;
           } else {
-            continue;
+            argumentPositionNullness.put(i, Nullness.NONNULL);
           }
         } else if (ASTHelpers.isSameType(
             param.type, Suppliers.JAVA_LANG_VOID_TYPE.get(state), state)) {
           // Temporarily treat a Void argument type as if it were @Nullable Void. Handling of Void
-          // without
-          // special-casing, as recommeded by JSpecify might: a) require generics support and, b)
-          // require
-          // checking that third-party libraries considered annotated adopt JSpecify semantics.
+          // without special-casing, as recommended by JSpecify might: a) require generics support
+          // and, b) require checking that third-party libraries considered annotated adopt
+          // JSpecify semantics.
           // See the suppression in https://github.com/uber/NullAway/pull/608 for an example of why
           // this is needed.
-          continue;
-        }
-        // we need to call paramHasNullableAnnotation here since the invoked method may be defined
-        // in a class file
-        if (!Nullness.paramHasNullableAnnotation(methodSymbol, i, config)) {
-          builder.add(i);
+          argumentPositionNullness.put(i, Nullness.NULLABLE);
+        } else {
+          // we need to call paramHasNullableAnnotation here since the invoked method may be defined
+          // in a class file
+          argumentPositionNullness.put(
+              i,
+              Nullness.paramHasNullableAnnotation(methodSymbol, i, config)
+                  ? Nullness.NULLABLE
+                  : Nullness.NONNULL);
         }
       }
-      nonNullPositions = builder.build();
     }
+
+    // Allow handlers to override the list of non-null argument positions
+    argumentPositionNullness =
+        handler.onOverrideMethodInvocationParametersNullability(
+            state.context, methodSymbol, isMethodAnnotated, argumentPositionNullness);
+
+    ImmutableSet<Integer> nonNullPositions =
+        argumentPositionNullness.entrySet().stream()
+            .filter(e -> e.getValue().equals(Nullness.NONNULL))
+            .map(e -> e.getKey())
+            .collect(ImmutableSet.toImmutableSet());
 
     // now actually check the arguments
     // NOTE: the case of an invocation on a possibly-null reference

nullaway/src/main/java/com/uber/nullaway/handlers/LibraryModelsHandler.java
@@ -24,6 +24,7 @@ package com.uber.nullaway.handlers;
 
 import static com.uber.nullaway.LibraryModels.MethodRef.methodRef;
 import static com.uber.nullaway.Nullness.NONNULL;
+import static com.uber.nullaway.Nullness.NULLABLE;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
@@ -43,9 +44,11 @@ import com.uber.nullaway.Config;
 import com.uber.nullaway.LibraryModels;
 import com.uber.nullaway.LibraryModels.MethodRef;
 import com.uber.nullaway.NullAway;
+import com.uber.nullaway.Nullness;
 import com.uber.nullaway.dataflow.AccessPath;
 import com.uber.nullaway.dataflow.AccessPathNullnessPropagation;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -77,28 +80,32 @@ public class LibraryModelsHandler extends BaseNoOpHandler {
   }
 
   @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetNonNullPositions(
-      NullAway analysis,
-      VisitorState state,
-      Symbol.MethodSymbol methodSymbol,
-      List<? extends ExpressionTree> actualParams,
-      ImmutableSet<Integer> nonNullPositions) {
-    return Sets.union(
-            nonNullPositions, getOptLibraryModels(state.context).nonNullParameters(methodSymbol))
-        .immutableCopy();
-  }
-
-  @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetExplicitlyNullablePositions(
+  public Map<Integer, Nullness> onOverrideMethodInvocationParametersNullability(
       Context context,
       Symbol.MethodSymbol methodSymbol,
-      ImmutableSet<Integer> explicitlyNullablePositions) {
-    return Sets.union(
-            Sets.difference(
-                explicitlyNullablePositions,
-                getOptLibraryModels(context).nonNullParameters(methodSymbol)),
-            getOptLibraryModels(context).explicitlyNullableParameters(methodSymbol))
-        .immutableCopy();
+      boolean isAnnotated,
+      Map<Integer, Nullness> argumentPositionNullness) {
+    OptimizedLibraryModels optimizedLibraryModels = getOptLibraryModels(context);
+    ImmutableSet<Integer> nullableParamsFromModel =
+        optimizedLibraryModels.explicitlyNullableParameters(methodSymbol);
+    ImmutableSet<Integer> nonNullParamsFromModel =
+        optimizedLibraryModels.nonNullParameters(methodSymbol);
+    Sets.SetView<Integer> intersection =
+        Sets.intersection(nonNullParamsFromModel, nullableParamsFromModel);
+    // Sanity check: $ nonNullParamsFromModel \cap nullableParamsFromModel $ should be empty
+    Preconditions.checkArgument(
+        intersection.isEmpty(),
+        String.format(
+            "Library models give conflicting nullability for the following parameters of method %s: %s",
+            methodSymbol.getQualifiedName().toString(),
+            Arrays.deepToString(intersection.toArray())));
+    for (Integer nullParam : nullableParamsFromModel) {
+      argumentPositionNullness.put(nullParam, NULLABLE);
+    }
+    for (Integer nonNullParam : nonNullParamsFromModel) {
+      argumentPositionNullness.put(nonNullParam, NONNULL);
+    }
+    return argumentPositionNullness;
   }
 
   @Override

guava-recent-unit-tests/src/test/java/com/uber/nullaway/guava/NullAwayGuavaParametricNullnessTests.java
@@ -117,4 +117,26 @@ public class NullAwayGuavaParametricNullnessTests {
             "}")
         .doTest();
   }
+
+  @Test
+  public void testFunctionMethodOverride() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import com.google.common.base.Function;",
+            "import javax.annotation.Nullable;",
+            "class Test {",
+            "    public static void testFoo() {",
+            "        Function<Object, Object> f = new Function<Object, Object>() {",
+            "            @Override",
+            "            @Nullable",
+            "            public Object apply(Object input) {",
+            "                return null;",
+            "             }",
+            "        };",
+            "    }",
+            "}")
+        .doTest();
+  }
 }

nullaway/src/main/java/com/uber/nullaway/dataflow/CoreNullnessStoreInitializer.java
@@ -3,7 +3,6 @@ package com.uber.nullaway.dataflow;
 import static com.uber.nullaway.Nullness.NONNULL;
 import static com.uber.nullaway.Nullness.NULLABLE;
 
-import com.google.common.collect.ImmutableSet;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.LambdaExpressionTree;
 import com.sun.source.tree.VariableTree;
@@ -15,7 +14,9 @@ import com.uber.nullaway.Config;
 import com.uber.nullaway.NullabilityUtil;
 import com.uber.nullaway.Nullness;
 import com.uber.nullaway.handlers.Handler;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import javax.annotation.Nullable;
 import javax.lang.model.element.Element;
@@ -93,9 +94,18 @@ class CoreNullnessStoreInitializer extends NullnessStoreInitializer {
     Symbol.MethodSymbol fiMethodSymbol = NullabilityUtil.getFunctionalInterfaceMethod(code, types);
     com.sun.tools.javac.util.List<Symbol.VarSymbol> fiMethodParameters =
         fiMethodSymbol.getParameters();
-    ImmutableSet<Integer> nullableParamsFromHandler =
-        handler.onUnannotatedInvocationGetExplicitlyNullablePositions(
-            context, fiMethodSymbol, ImmutableSet.of());
+    Map<Integer, Nullness> fiArgumentPositionNullness = new LinkedHashMap<>();
+    final boolean isFIAnnotated = !classAnnotationInfo.isSymbolUnannotated(fiMethodSymbol, config);
+    if (isFIAnnotated) {
+      for (int i = 0; i < fiMethodParameters.size(); i++) {
+        fiArgumentPositionNullness.put(
+            i,
+            Nullness.hasNullableAnnotation(fiMethodParameters.get(i), config) ? NULLABLE : NONNULL);
+      }
+    }
+    fiArgumentPositionNullness =
+        handler.onOverrideMethodInvocationParametersNullability(
+            context, fiMethodSymbol, isFIAnnotated, fiArgumentPositionNullness);
 
     for (int i = 0; i < parameters.size(); i++) {
       LocalVariableNode param = parameters.get(i);
@@ -111,15 +121,7 @@ class CoreNullnessStoreInitializer extends NullnessStoreInitializer {
         // treat as non-null
         assumed = NONNULL;
       } else {
-        if (classAnnotationInfo.isSymbolUnannotated(fiMethodSymbol, config)) {
-          // assume parameter is non-null unless handler tells us otherwise
-          assumed = nullableParamsFromHandler.contains(i) ? NULLABLE : NONNULL;
-        } else {
-          assumed =
-              Nullness.hasNullableAnnotation(fiMethodParameters.get(i), config)
-                  ? NULLABLE
-                  : NONNULL;
-        }
+        assumed = fiArgumentPositionNullness.getOrDefault(i, NONNULL);
       }
       result.setInformation(AccessPath.fromLocal(param), assumed);
     }

nullaway/src/main/java/com/uber/nullaway/handlers/BaseNoOpHandler.java
@@ -36,12 +36,14 @@ import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.util.Context;
 import com.uber.nullaway.ErrorMessage;
 import com.uber.nullaway.NullAway;
+import com.uber.nullaway.Nullness;
 import com.uber.nullaway.dataflow.AccessPath;
 import com.uber.nullaway.dataflow.AccessPathNullnessAnalysis;
 import com.uber.nullaway.dataflow.AccessPathNullnessPropagation;
 import com.uber.nullaway.dataflow.NullnessStore;
 import com.uber.nullaway.dataflow.cfg.NullAwayCFGBuilder;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import org.checkerframework.nullaway.dataflow.cfg.UnderlyingAST;
@@ -107,15 +109,6 @@ public abstract class BaseNoOpHandler implements Handler {
   }
 
   @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetExplicitlyNullablePositions(
-      Context context,
-      Symbol.MethodSymbol methodSymbol,
-      ImmutableSet<Integer> explicitlyNullablePositions) {
-    // NoOp
-    return explicitlyNullablePositions;
-  }
-
-  @Override
   public boolean onUnannotatedInvocationGetExplicitlyNonNullReturn(
       Symbol.MethodSymbol methodSymbol, boolean explicitlyNonNullReturn) {
     // NoOp
@@ -123,14 +116,13 @@ public abstract class BaseNoOpHandler implements Handler {
   }
 
   @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetNonNullPositions(
-      NullAway analysis,
-      VisitorState state,
+  public Map<Integer, Nullness> onOverrideMethodInvocationParametersNullability(
+      Context context,
       Symbol.MethodSymbol methodSymbol,
-      List<? extends ExpressionTree> actualParams,
-      ImmutableSet<Integer> nonNullPositions) {
+      boolean isAnnotated,
+      Map<Integer, Nullness> argumentPositionNullness) {
     // NoOp
-    return nonNullPositions;
+    return argumentPositionNullness;
   }
 
   @Override

nullaway/src/main/java/com/uber/nullaway/handlers/CompositeHandler.java
@@ -37,12 +37,14 @@ import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.util.Context;
 import com.uber.nullaway.ErrorMessage;
 import com.uber.nullaway.NullAway;
+import com.uber.nullaway.Nullness;
 import com.uber.nullaway.dataflow.AccessPath;
 import com.uber.nullaway.dataflow.AccessPathNullnessAnalysis;
 import com.uber.nullaway.dataflow.AccessPathNullnessPropagation;
 import com.uber.nullaway.dataflow.NullnessStore;
 import com.uber.nullaway.dataflow.cfg.NullAwayCFGBuilder;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import org.checkerframework.nullaway.dataflow.cfg.UnderlyingAST;
@@ -121,19 +123,6 @@ class CompositeHandler implements Handler {
   }
 
   @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetExplicitlyNullablePositions(
-      Context context,
-      Symbol.MethodSymbol methodSymbol,
-      ImmutableSet<Integer> explicitlyNullablePositions) {
-    for (Handler h : handlers) {
-      explicitlyNullablePositions =
-          h.onUnannotatedInvocationGetExplicitlyNullablePositions(
-              context, methodSymbol, explicitlyNullablePositions);
-    }
-    return explicitlyNullablePositions;
-  }
-
-  @Override
   public boolean onUnannotatedInvocationGetExplicitlyNonNullReturn(
       Symbol.MethodSymbol methodSymbol, boolean explicitlyNonNullReturn) {
     for (Handler h : handlers) {
@@ -145,18 +134,17 @@ class CompositeHandler implements Handler {
   }
 
   @Override
-  public ImmutableSet<Integer> onUnannotatedInvocationGetNonNullPositions(
-      NullAway analysis,
-      VisitorState state,
+  public Map<Integer, Nullness> onOverrideMethodInvocationParametersNullability(
+      Context context,
       Symbol.MethodSymbol methodSymbol,
-      List<? extends ExpressionTree> actualParams,
-      ImmutableSet<Integer> nonNullPositions) {
+      boolean isAnnotated,
+      Map<Integer, Nullness> argumentPositionNullness) {
     for (Handler h : handlers) {
-      nonNullPositions =
-          h.onUnannotatedInvocationGetNonNullPositions(
-              analysis, state, methodSymbol, actualParams, nonNullPositions);
+      argumentPositionNullness =
+          h.onOverrideMethodInvocationParametersNullability(
+              context, methodSymbol, isAnnotated, argumentPositionNullness);
     }
-    return nonNullPositions;
+    return argumentPositionNullness;
   }
 
   @Override

commit d34982ad1718c39d2602c5bb2c414f12f35e8eaa

Optimizations for parameter nullness handler / overriding (#646)
Mostly (?) addresses #645.

Since this code seems to be performance critical, we should consider shifting our representation of parameter nullability from a `Map<Integer,Nullness>` to an `ArrayList<Nullness>`.  But that is a more intrusive change.  These changes seems be enough to address the performance regression from #639 (to be confirmed with a full run of perf regression tests).

nullaway/src/main/java/com/uber/nullaway/NullAway.java
@@ -1503,16 +1503,14 @@ public class NullAway extends BugChecker
         handler.onOverrideMethodInvocationParametersNullability(
             state.context, methodSymbol, isMethodAnnotated, argumentPositionNullness);
 
-    ImmutableSet<Integer> nonNullPositions =
-        argumentPositionNullness.entrySet().stream()
-            .filter(e -> e.getValue().equals(Nullness.NONNULL))
-            .map(e -> e.getKey())
-            .collect(ImmutableSet.toImmutableSet());
-
     // now actually check the arguments
     // NOTE: the case of an invocation on a possibly-null reference
     // is handled by matchMemberSelect()
-    for (int argPos : nonNullPositions) {
+    for (Map.Entry<Integer, Nullness> entry : argumentPositionNullness.entrySet()) {
+      if (!entry.getValue().equals(Nullness.NONNULL)) {
+        continue;
+      }
+      int argPos = entry.getKey();
       ExpressionTree actual = null;
       boolean mayActualBeNull = false;
       if (argPos == formalParams.size() - 1 && methodSymbol.isVarArgs()) {

nullaway/src/main/java/com/uber/nullaway/handlers/LibraryModelsHandler.java
@@ -29,7 +29,6 @@ import static com.uber.nullaway.Nullness.NULLABLE;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.Sets;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
@@ -48,7 +47,7 @@ import com.uber.nullaway.Nullness;
 import com.uber.nullaway.dataflow.AccessPath;
 import com.uber.nullaway.dataflow.AccessPathNullnessPropagation;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -90,19 +89,20 @@ public class LibraryModelsHandler extends BaseNoOpHandler {
         optimizedLibraryModels.explicitlyNullableParameters(methodSymbol);
     ImmutableSet<Integer> nonNullParamsFromModel =
         optimizedLibraryModels.nonNullParameters(methodSymbol);
-    Sets.SetView<Integer> intersection =
-        Sets.intersection(nonNullParamsFromModel, nullableParamsFromModel);
-    // Sanity check: $ nonNullParamsFromModel \cap nullableParamsFromModel $ should be empty
-    Preconditions.checkArgument(
-        intersection.isEmpty(),
-        String.format(
-            "Library models give conflicting nullability for the following parameters of method %s: %s",
-            methodSymbol.getQualifiedName().toString(),
-            Arrays.deepToString(intersection.toArray())));
+    // For sanity check: $ nonNullParamsFromModel \cap nullableParamsFromModel $ should be empty
+    Set<Integer> allPositions = new HashSet<>();
     for (Integer nullParam : nullableParamsFromModel) {
+      allPositions.add(nullParam);
       argumentPositionNullness.put(nullParam, NULLABLE);
     }
     for (Integer nonNullParam : nonNullParamsFromModel) {
+      if (!allPositions.add(nonNullParam)) {
+        // position was already marked as nullable
+        throw new IllegalStateException(
+            String.format(
+                "Library models give conflicting nullability for the following parameter of method %s: %s",
+                methodSymbol.getQualifiedName().toString(), nonNullParam.toString()));
+      }
       argumentPositionNullness.put(nonNullParam, NONNULL);
     }
     return argumentPositionNullness;