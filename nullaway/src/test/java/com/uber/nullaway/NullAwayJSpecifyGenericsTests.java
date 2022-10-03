98f7fa63f7456ff16e419285adfa9fd08660d00f
Manu Sridharan <msridhar@gmail.com>
2022-10-03 13:35:32 -0700

    more tests

--- nullaway/src/test/java/com/uber/nullaway/NullAwayJSpecifyGenericsTests.java
+++ nullaway/src/test/java/com/uber/nullaway/NullAwayJSpecifyGenericsTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class NullAwayJSpecifyGenericsTests extends NullAwayTestsBase {
 
   @Test
-  public void basicTypeParamSubtyping() {
+  public void basicTypeParamInstantiation() {
     defaultCompilationHelper
         .addSourceLines(
             "Test.java",
@@ -19,14 +19,19 @@ public class NullAwayJSpecifyGenericsTests extends NullAwayTestsBase {
             "    }",
             "    // BUG: Diagnostic contains: Generic type parameter",
             "    static void testBadNonNull(NonNullTypeParam<@Nullable String> t) {",
-            "        // 2 reports here?",
             "        // BUG: Diagnostic contains: Generic type parameter",
-            "        NonNullTypeParam<@Nullable String> t2 = new NonNullTypeParam<@Nullable String>();",
+            "        NonNullTypeParam<@Nullable String> t2 = null;",
+            "        // BUG: Diagnostic contains: Generic type parameter",
+            "        t2 = new NonNullTypeParam<@Nullable String>();",
+            "        // BUG: Diagnostic contains: Generic type parameter",
+            "        testBadNonNull(new NonNullTypeParam<@Nullable String>());",
             "    }",
             "    static void testOkNullable(NullableTypeParam<String> t1, NullableTypeParam<@Nullable String> t2) {",
             "        NullableTypeParam<String> t3 = new NullableTypeParam<String>();",
             "        NullableTypeParam<@Nullable String> t4 = new NullableTypeParam<@Nullable String>();",
             "    }",
+            "    // BUG: Diagnostic contains: Generic type parameter",
+            "    static class InvalidSubclass extends NonNullTypeParam<@Nullable String> {}",
             "}")
         .doTest();
   }
