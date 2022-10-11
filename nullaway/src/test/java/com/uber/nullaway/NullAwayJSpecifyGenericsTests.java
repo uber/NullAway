4cb51d998ed192892d02908fa62535ea361d751a
NikitaAware <nikita.aware@email.ucr.edu>
2022-10-10 20:06:09 -0700

    restructure generics tests

--- nullaway/src/test/java/com/uber/nullaway/NullAwayJSpecifyGenericsTests.java
+++ nullaway/src/test/java/com/uber/nullaway/NullAwayJSpecifyGenericsTests.java
@@ -15,15 +15,35 @@ public class NullAwayJSpecifyGenericsTests extends NullAwayTestsBase {
             "    static class NonNullTypeParam<E> {}",
             "    static class NullableTypeParam<E extends @Nullable Object> {}",
             "    static interface NonNullTypeParamInterface<E>{}",
-            "    static void testOkNonNull(NonNullTypeParam<String> t) {",
-            "        NonNullTypeParam<String> t2 = new NonNullTypeParam<String>();",
-            "    }",
             "    // BUG: Diagnostic contains: Generic type parameter",
             "    static void testBadNonNull(NonNullTypeParam<@Nullable String> t) {",
             "        // BUG: Diagnostic contains: Generic type parameter",
             "        NonNullTypeParam<@Nullable String> t2 = null;",
+            "    }",
+            "    // BUG: Diagnostic contains: Generic type parameter",
+            "    static class InvalidSubclass extends NonNullTypeParam<@Nullable String> {}",
+            "    // BUG: Diagnostic contains: Generic type parameter",
+            "    static class InvalidInterfaceImplementation implements NonNullTypeParamInterface<@Nullable String> {}",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void ConstructorTypeParamInstantiation() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import org.jspecify.nullness.Nullable;",
+            "class Test {",
+            "    static class NonNullTypeParam<E> {}",
+            "    static class NullableTypeParam<E extends @Nullable Object> {}",
+            "    static void testOkNonNull(NonNullTypeParam<String> t) {",
+            "        NonNullTypeParam<String> t2 = new NonNullTypeParam<String>();",
+            "    }",
+            "    static void testBadNonNull(NonNullTypeParam<String> t) {",
             "        // BUG: Diagnostic contains: Generic type parameter",
-            "        t2 = new NonNullTypeParam<@Nullable String>();",
+            "       NonNullTypeParam<String> t2 = new NonNullTypeParam<@Nullable String>();",
             "        // BUG: Diagnostic contains: Generic type parameter",
             "        testBadNonNull(new NonNullTypeParam<@Nullable String>());",
             "    }",
@@ -31,15 +51,59 @@ public class NullAwayJSpecifyGenericsTests extends NullAwayTestsBase {
             "        NullableTypeParam<String> t3 = new NullableTypeParam<String>();",
             "        NullableTypeParam<@Nullable String> t4 = new NullableTypeParam<@Nullable String>();",
             "    }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void ExtendedClassTypeParamInstantiation() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import org.jspecify.nullness.Nullable;",
+            "class Test {",
+            "    static class NonNullTypeParam<E> {}",
             "    // BUG: Diagnostic contains: Generic type parameter",
             "    static class InvalidSubclass extends NonNullTypeParam<@Nullable String> {}",
-            "    // BUG: Diagnostic contains: Generic type parameter",
-            "    static class InvalidInterfaceImplementation implements NonNullTypeParamInterface<@Nullable String> {}",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void SubClassTypeParamInstantiation() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import org.jspecify.nullness.Nullable;",
+            "class Test {",
+            "    static class NonNullTypeParam<E> {}",
+            "    static class NullableTypeParam<E extends @Nullable Object> {}",
             "    static class SuperClassForValidSubclass {",
             "        static class ValidSubclass extends NullableTypeParam<@Nullable String> {}",
             "        // BUG: Diagnostic contains: Generic type parameter",
             "        static class InvalidSubclass extends NonNullTypeParam<@Nullable String> {}",
             "    }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void InterfaceImplementationTypeParamInstantiation() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import org.jspecify.nullness.Nullable;",
+            "class Test {",
+            "    static class NonNullTypeParam<E> {}",
+            "    static class NullableTypeParam<E extends @Nullable Object> {}",
+            "    static interface NonNullTypeParamInterface<E>{}",
+            "    static interface NullableTypeParamInterface<E extends @Nullable Object>{}",
+            "    // BUG: Diagnostic contains: Generic type parameter",
+            "    static class InvalidInterfaceImplementation implements NonNullTypeParamInterface<@Nullable String> {}",
+            "    static class ValidInterfaceImplementation implements NullableTypeParamInterface<String> {}",
             "}")
         .doTest();
   }
