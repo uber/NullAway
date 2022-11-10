8308cf6cab00287661a55ede7488c4c4d74f7964
Manu Sridharan <msridhar@gmail.com>
2022-11-10 14:49:19 -0800

    code cleanup

--- nullaway/src/main/java/com/uber/nullaway/GenericsChecks.java
+++ nullaway/src/main/java/com/uber/nullaway/GenericsChecks.java
@@ -2,13 +2,15 @@ package com.uber.nullaway;
 
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.AnnotatedTypeTree;
+import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ParameterizedTypeTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Attribute;
 import com.sun.tools.javac.code.Type;
-import com.sun.tools.javac.tree.JCTree;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** Methods for performing checks related to generic types and nullability. */
 public class GenericsChecks {
@@ -18,44 +20,8 @@ public class GenericsChecks {
   }
 
   /**
-   * Checks if the type arguments with a {@code @Nullable} annotation in an instantiated type have a
-   * {@code @Nullable} upper bound in the declaration, and reports an error otherwise.
-   *
-   * @param state the visitor state
-   * @param analysis the analysis object
-   * @param config the analysis config
-   * @param nullableTypeArguments indices of {@code Nullable} type arguments
-   * @param baseTypeArguments list of type variables (with bounds) in the declared type
-   */
-  private static void checkNullableTypeArgsAgainstUpperBounds(
-      VisitorState state,
-      NullAway analysis,
-      Config config,
-      HashMap<Integer, Tree> nullableTypeArguments,
-      com.sun.tools.javac.util.List<Type> baseTypeArguments) {
-    for (int i = 0; i < baseTypeArguments.size(); i++) {
-      if (nullableTypeArguments.containsKey(i)) {
-
-        Type upperBound = baseTypeArguments.get(i).getUpperBound();
-        com.sun.tools.javac.util.List<Attribute.TypeCompound> annotationMirrors =
-            upperBound.getAnnotationMirrors();
-        boolean hasNullableAnnotation =
-            Nullness.hasNullableAnnotation(annotationMirrors.stream(), config);
-        // if base type argument does not have @Nullable annotation then the instantiation is
-        // invalid
-        if (!hasNullableAnnotation) {
-          invalidInstantiationError(nullableTypeArguments.get(i), state, analysis);
-        }
-      }
-    }
-  }
-
-  /**
    * Checks that for an instantiated generic type, {@code @Nullable} types are only used for type
-   * variables that have a {@code @Nullable} upper bound. Similar to {@link
-   * #checkInstantiationForParameterizedTypedTree(ParameterizedTypeTree, VisitorState, NullAway,
-   * Config)} but specialized for when the instantiated type is represented as a {@link
-   * ParameterizedTypeTree}.
+   * variables that have a {@code @Nullable} upper bound.
    *
    * @param tree the tree representing the instantiated type
    * @param state visitor state
@@ -68,14 +34,16 @@ public class GenericsChecks {
       return;
     }
     List<? extends Tree> typeArguments = tree.getTypeArguments();
-    HashMap<Integer, Tree> nullableTypeArguments = new HashMap<Integer, Tree>();
+    Map<Integer, Tree> nullableTypeArguments = new HashMap<>();
     for (int i = 0; i < typeArguments.size(); i++) {
-      if (typeArguments.get(i).getClass().equals(JCTree.JCAnnotatedType.class)) {
-        JCTree.JCAnnotatedType annotatedType = (JCTree.JCAnnotatedType) typeArguments.get(i);
-        for (JCTree.JCAnnotation annotation : annotatedType.getAnnotations()) {
-          Attribute.Compound attribute = annotation.attribute;
-          if (attribute.toString().equals("@org.jspecify.nullness.Nullable")) {
-            nullableTypeArguments.put(i, typeArguments.get(i));
+      Tree curTypeArg = typeArguments.get(i);
+      if (curTypeArg instanceof AnnotatedTypeTree) {
+        AnnotatedTypeTree annotatedType = (AnnotatedTypeTree) curTypeArg;
+        for (AnnotationTree annotation : annotatedType.getAnnotations()) {
+          Type annotationType = ASTHelpers.getType(annotation);
+          if (annotationType != null
+              && Nullness.isNullableAnnotation(annotationType.toString(), config)) {
+            nullableTypeArguments.put(i, curTypeArg);
             break;
           }
         }
@@ -87,17 +55,27 @@ public class GenericsChecks {
       return;
     }
     com.sun.tools.javac.util.List<Type> baseTypeArgs = baseType.tsym.type.getTypeArguments();
-    checkNullableTypeArgsAgainstUpperBounds(
-        state, analysis, config, nullableTypeArguments, baseTypeArgs);
+    for (int i = 0; i < baseTypeArgs.size(); i++) {
+      if (nullableTypeArguments.containsKey(i)) {
+
+        Type upperBound = baseTypeArgs.get(i).getUpperBound();
+        com.sun.tools.javac.util.List<Attribute.TypeCompound> annotationMirrors =
+            upperBound.getAnnotationMirrors();
+        boolean hasNullableAnnotation =
+            Nullness.hasNullableAnnotation(annotationMirrors.stream(), config);
+        // if base type argument does not have @Nullable annotation then the instantiation is
+        // invalid
+        if (!hasNullableAnnotation) {
+          invalidInstantiationError(nullableTypeArguments.get(i), state, analysis);
+        }
+      }
+    }
     // recursive check for nested parameterized types
     for (int i = 0; i < typeArguments.size(); i++) {
-      if (typeArguments.get(i).getClass().equals(JCTree.JCTypeApply.class)) {
+      if (typeArguments.get(i) instanceof ParameterizedTypeTree) {
         ParameterizedTypeTree parameterizedTypeTreeForTypeArgument =
             (ParameterizedTypeTree) typeArguments.get(i);
-        Type argumentType = ASTHelpers.getType(parameterizedTypeTreeForTypeArgument);
-        if (argumentType != null
-            && argumentType.getTypeArguments() != null
-            && argumentType.getTypeArguments().length() > 0) { // Nested generics
+        if (parameterizedTypeTreeForTypeArgument.getTypeArguments().size() > 0) {
           checkInstantiationForParameterizedTypedTree(
               parameterizedTypeTreeForTypeArgument, state, analysis, config);
         }
@@ -110,7 +88,7 @@ public class GenericsChecks {
     ErrorMessage errorMessage =
         new ErrorMessage(
             ErrorMessage.MessageTypes.TYPE_PARAMETER_CANNOT_BE_NULLABLE,
-            "Generic type parameter cannot be @Nullable");
+            "Generic type parameter cannot be @Nullable, as type variable does not have a @Nullable upper bound");
     state.reportMatch(
         errorBuilder.createErrorDescription(
             errorMessage, analysis.buildDescription(tree), state, null));
