33d6a5c385694f6cc00b29e25b5325477d006fed
Lázaro Clapp <lazaro.clapp@gmail.com>
2023-01-13 23:57:16 -0500

    Follow up comment clarification and link to issue

--- nullaway/src/main/java/com/uber/nullaway/fixserialization/location/AbstractSymbolLocation.java
+++ nullaway/src/main/java/com/uber/nullaway/fixserialization/location/AbstractSymbolLocation.java
@@ -53,7 +53,12 @@ public abstract class AbstractSymbolLocation implements SymbolLocation {
             + ".");
     this.type = type;
     this.enclosingClass = castToNonNull(ASTHelpers.enclosingClass(target));
-    // URIs for classes are still serialized. Can be revisited in follow up later.
+    // We currently serialize the URI for the classfile if the URI for the sourcefile is not available,
+    // but only if said URI corresponds to a "file:" or "jimfs:" scheme (i.e. not "jar:"). It's likely that 
+    // this is no longer needed and should be removed as a follow up:  
+    // https://github.com/uber/NullAway/issues/716
+    // Leaving this workaround up temporarily for the sake of experiments with version 
+    // `1.3.6-alpha-N` of the auto-annotator.
     URI pathInURI =
         enclosingClass.sourcefile != null
             ? enclosingClass.sourcefile.toUri()
