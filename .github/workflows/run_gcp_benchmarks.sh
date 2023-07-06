860ad36b15cb4b6879919a3fafc9e4b49ae4a5b2
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-05 22:53:09 -0500

    Update run_gcp_benchmarks.sh

--- .github/workflows/run_gcp_benchmarks.sh
+++ .github/workflows/run_gcp_benchmarks.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# Assign command line argument to a variable
+# This script is responsible for running benchmarks for a GitHub pull request and the main branch on Google Cloud Compute Engine (GCCE).
+
+# Accepting a command line argument
 CMD="$1"
 
-# Setup Directory and Copy Bash Scripts
+# Setting up a new directory named after the branch on GCCE and copying bash scripts into this directory
+# The ssh command is made executable and then run with a command to export the branch name and make a new directory on the GCCE
 chmod +x ./.github/workflows/gcloud_ssh.sh
 ./.github/workflows/gcloud_ssh.sh "export BRANCH_NAME=${BRANCH_NAME} && mkdir $BRANCH_NAME"
+
+# Using gcloud compute scp to copy the bash scripts that will run the benchmarks onto the GCCE
 gcloud compute scp ./.github/workflows/run_pr_benchmarks.sh root@instance-1:$BRANCH_NAME/ --zone=us-central1-a
 gcloud compute scp ./.github/workflows/run_main_benchmarks.sh root@instance-1:$BRANCH_NAME/ --zone=us-central1-a
 
-# Run JMH benchmarks for PR Branch
+# Running the benchmark script for the pull request branch on GCCE
 ./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && export REPO_NAME=${REPO_NAME} && chmod +x $BRANCH_NAME/run_pr_benchmarks.sh && $BRANCH_NAME/run_pr_benchmarks.sh"
 
-# Copy exported PR benchmark file from Google Cloud Compute Engine
+# Copying the benchmark results from GCCE back to the Github runner for the PR branch
 gcloud compute scp root@instance-1:$BRANCH_NAME/pr/NullAway/jmh/build/results/jmh/results.txt ./pr_text.txt --zone=us-central1-a
 
-# Run JMH benchmarks for Main Branch
+# Running the benchmark script for the main branch on GCCE
 ./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && chmod +x $BRANCH_NAME/run_main_benchmarks.sh && $BRANCH_NAME/run_main_benchmarks.sh "
 
-# Copy exported Main Benchmark file from Google Cloud Compute Engine 
+# Copying the benchmark results from GCCE back to the Github runner for the main branch
 gcloud compute scp root@instance-1:$BRANCH_NAME/main/NullAway/jmh/build/results/jmh/results.txt ./main_text.txt --zone=us-central1-a
