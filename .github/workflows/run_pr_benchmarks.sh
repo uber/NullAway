619176a28c4cd26728cf0cc34cf7b567f70f0a43
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-05 22:55:11 -0500

    Update run_pr_benchmarks.sh

--- .github/workflows/run_pr_benchmarks.sh
+++ .github/workflows/run_pr_benchmarks.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# This script is responsible for cloning a specific branch (the PR branch) from a GitHub repository and running Java Microbenchmark Harness (JMH) benchmarks.
+
+# Change directory to the one named after the branch name. 
 cd $BRANCH_NAME/
+
+# Create a new directory called 'pr' and navigate into it
 mkdir pr
 cd pr/
+
+# Clone the specific branch from the GitHub repository. 
+# The repository and branch are specified by the REPO_NAME and BRANCH_NAME variables.
 git clone --branch $BRANCH_NAME --single-branch git@github.com:$REPO_NAME.git
+
+# Change directory into the newly cloned repository
 cd NullAway/
+
+# Run the JMH benchmarks using the Gradle Wrapper. 
 ./gradlew jmh
