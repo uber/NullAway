9cded48d6291f99ff42809e1dd0ec6237605f9a2
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-05 22:54:20 -0500

    Update run_main_benchmarks.sh

--- .github/workflows/run_main_benchmarks.sh
+++ .github/workflows/run_main_benchmarks.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# This script is responsible for cloning the main repository from GitHub and running Java Microbenchmark Harness (JMH) benchmarks.
+
+# Change directory to the one named after the branch name. 
 cd $BRANCH_NAME/
+
+# Create a new directory called 'main' and navigate into it
 mkdir main
 cd main/
+
+# Clone the main repository from GitHub
 git clone git@github.com:Uber/NullAway.git
+
+# Change directory into the newly cloned repository
 cd NullAway/
+
+# Run the JMH benchmarks using the Gradle Wrapper. 
 ./gradlew jmh
