11b65c9f68fed00bf34ded7df974c05f49551ebf
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-14 19:06:00 -0500

    Update gcloud_ssh.sh

--- .github/workflows/gcloud_ssh.sh
+++ .github/workflows/gcloud_ssh.sh
@@ -7,7 +7,4 @@ PROJECT_ID="ucr-ursa-major-sridharan-lab"
 ZONE="us-central1-a"
 USER="root"
 INSTANCE="nullway-jmh"
-
-# Use the gcloud compute ssh command to run a command ($1) on the specified Google Cloud instance
-# $1 is a positional parameter that represents the first argument given when calling this script
 gcloud compute ssh --project=$PROJECT_ID --zone=$ZONE $USER@$INSTANCE --command="$1"
