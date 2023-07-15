9b99d3a6d3fd4b2608803784df012c96ccf0c87f
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-14 19:11:35 -0500

    Update run_gcp_benchmarks.sh

--- .github/workflows/run_gcp_benchmarks.sh
+++ .github/workflows/run_gcp_benchmarks.sh
@@ -3,8 +3,6 @@
 # This script is responsible for running benchmarks for a GitHub pull request and the main branch on Google Cloud Compute Engine (GCCE).
 
 
-# Setting up a new directory named after the branch on GCCE and copying bash scripts into this directory
-# The ssh command is made executable and then run with a command to export the branch name and make a new directory on the GCCE
 chmod +x ./.github/workflows/gcloud_ssh.sh
 ./.github/workflows/gcloud_ssh.sh "export BRANCH_NAME=${BRANCH_NAME} && mkdir $BRANCH_NAME"
 
@@ -13,13 +11,10 @@ gcloud compute scp ./.github/workflows/run_pr_benchmarks.sh root@nullway-jmh:$BR
 gcloud compute scp ./.github/workflows/run_main_benchmarks.sh root@nullway-jmh:$BRANCH_NAME/ --zone=us-central1-a
 
 # Running the benchmark script for the pull request branch on GCCE
-./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && export REPO_NAME=${REPO_FULL_NAME} && chmod +x $BRANCH_NAME/run_pr_benchmarks.sh && $BRANCH_NAME/run_pr_benchmarks.sh"
+./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && export REPO_NAME=${REPO_FULL_NAME} && chmod +x $BRANCH_NAME/run_pr_benchmarks.sh && $BRANCH_NAME/run_pr_benchmarks.sh && cd && chmod +x $BRANCH_NAME/run_main_benchmarks.sh && $BRANCH_NAME/run_main_benchmarks.sh"
 
 # Copying the benchmark results from GCCE back to the Github runner for the PR branch
 gcloud compute scp root@instance-1:$BRANCH_NAME/pr/NullAway/jmh/build/results/jmh/results.txt ./pr_text.txt --zone=us-central1-a
-
-# Running the benchmark script for the main branch on GCCE
-./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && chmod +x $BRANCH_NAME/run_main_benchmarks.sh && $BRANCH_NAME/run_main_benchmarks.sh "
 
 # Copying the benchmark results from GCCE back to the Github runner for the main branch
 gcloud compute scp root@instance-1:$BRANCH_NAME/main/NullAway/jmh/build/results/jmh/results.txt ./main_text.txt --zone=us-central1-a
