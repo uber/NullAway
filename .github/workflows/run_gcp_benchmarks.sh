90f1cb130e0dee48dcc145d5d43d0db1d171a2e9
armughan11 <52311490+armughan11@users.noreply.github.com>
2023-07-14 21:02:50 -0500

    Update run_gcp_benchmarks.sh

--- .github/workflows/run_gcp_benchmarks.sh
+++ .github/workflows/run_gcp_benchmarks.sh
@@ -14,7 +14,7 @@ gcloud compute scp ./.github/workflows/run_main_benchmarks.sh root@nullway-jmh:$
 ./.github/workflows/gcloud_ssh.sh " export BRANCH_NAME=${BRANCH_NAME} && export REPO_NAME=${REPO_FULL_NAME} && chmod +x $BRANCH_NAME/run_pr_benchmarks.sh && $BRANCH_NAME/run_pr_benchmarks.sh && cd && chmod +x $BRANCH_NAME/run_main_benchmarks.sh && $BRANCH_NAME/run_main_benchmarks.sh"
 
 # Copying the benchmark results from GCCE back to the Github runner for the PR branch
-gcloud compute scp root@instance-1:$BRANCH_NAME/pr/NullAway/jmh/build/results/jmh/results.txt ./pr_text.txt --zone=us-central1-a
+gcloud compute scp root@nullway-jmh:$BRANCH_NAME/pr/NullAway/jmh/build/results/jmh/results.txt ./pr_text.txt --zone=us-central1-a
 
 # Copying the benchmark results from GCCE back to the Github runner for the main branch
-gcloud compute scp root@instance-1:$BRANCH_NAME/main/NullAway/jmh/build/results/jmh/results.txt ./main_text.txt --zone=us-central1-a
+gcloud compute scp root@nullway-jmh:$BRANCH_NAME/main/NullAway/jmh/build/results/jmh/results.txt ./main_text.txt --zone=us-central1-a
