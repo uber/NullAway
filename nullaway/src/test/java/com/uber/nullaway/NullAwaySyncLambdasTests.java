b3da601f6e11a5c3529d74240848e6ca4674ab6a
Manu Sridharan <msridhar@gmail.com>
2024-03-30 09:26:51 -0700

    test case

---
+++ nullaway/src/test/java/com/uber/nullaway/NullAwaySyncLambdasTests.java
@@ -0,0 +1,34 @@
+package com.uber.nullaway;
+
+import org.junit.Test;
+
+/**
+ * Tests for cases where lambdas or anonymous class methods are invoked nearly synchronously, so it
+ * is reasonable to propagat more nullability information to their bodies.
+ */
+public class NullAwaySyncLambdasTests extends NullAwayTestsBase {
+  @Test
+  public void testForEach() {
+    defaultCompilationHelper
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import java.util.Map;",
+            "import java.util.HashMap;",
+            "import org.jspecify.annotations.Nullable;",
+            "public class Test {",
+            "    private @Nullable Map<Object, Object> target;",
+            "    private @Nullable Map<Object, Object> resolved;",
+            "    public void initialize() {",
+            "        if (this.target == null) {",
+            "            throw new IllegalArgumentException();",
+            "        }",
+            "        this.resolved = new HashMap<>();",
+            "        this.target.forEach((key, value) -> {",
+            "            this.resolved.put(key, value);",
+            "        });",
+            "    }",
+            "}")
+        .doTest();
+  }
+}
