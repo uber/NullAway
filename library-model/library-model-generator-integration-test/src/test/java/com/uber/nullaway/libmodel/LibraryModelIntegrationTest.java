4186ca7066bd61535fbdbeb990041d6e7e250503
Manu Sridharan <msridhar@gmail.com>
2024-06-04 11:43:14 -0700

    comment

--- library-model/library-model-generator-integration-test/src/test/java/com/uber/nullaway/libmodel/LibraryModelIntegrationTest.java
+++ library-model/library-model-generator-integration-test/src/test/java/com/uber/nullaway/libmodel/LibraryModelIntegrationTest.java
@@ -8,6 +8,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+/**
+ * Integration test for library model support. The library models are contained in the jar for the
+ * test-library-model-generator project, as a stubx file. These tests ensure that NullAway correctly
+ * loads the stubx file and reports the right errors based on those models.
+ */
 public class LibraryModelIntegrationTest {
 
   @Rule public final TemporaryFolder temporaryFolder = new TemporaryFolder();
