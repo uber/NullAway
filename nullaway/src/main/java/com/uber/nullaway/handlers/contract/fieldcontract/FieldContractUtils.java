1c5610cbdb6188420c5f06673624179b02e8c5d8
Manu Sridharan <msridhar@gmail.com>
2024-09-30 15:27:54 -0700

    comments

--- nullaway/src/main/java/com/uber/nullaway/handlers/contract/fieldcontract/FieldContractUtils.java
+++ nullaway/src/main/java/com/uber/nullaway/handlers/contract/fieldcontract/FieldContractUtils.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2024 Uber Technologies, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.uber.nullaway.handlers.contract.fieldcontract;
 
 import static com.uber.nullaway.NullabilityUtil.castToNonNull;
@@ -14,6 +35,17 @@ import java.util.Set;
 
 public class FieldContractUtils {
 
+  /**
+   * Checks that the fields mentioned in the annotation of the overridden method are also mentioned
+   * in the annotation of the overriding method. If not, reports an error.
+   *
+   * @param annotName name of the annotation
+   * @param overridingFieldNames set of fields mentioned in the overriding method's annotation
+   * @param analysis NullAway instance
+   * @param state the visitor state
+   * @param tree tree for the overriding method
+   * @param overriddenMethod the method that is being overridden
+   */
   public static void ensureStrictPostConditionInheritance(
       String annotName,
       Set<String> overridingFieldNames,
