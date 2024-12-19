cf4d30feb596042276a309b42822838b7e23519b
Manu Sridharan <msridhar@gmail.com>
2024-12-18 21:01:12 -0800

    WIP

--- nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
+++ nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
@@ -26,6 +26,7 @@ import com.sun.tools.javac.code.Attribute;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.TargetType;
 import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.tree.JCTree;
 import com.uber.nullaway.CodeAnnotationInfo;
 import com.uber.nullaway.Config;
@@ -624,7 +625,8 @@ public final class GenericsChecks {
         enclosingType = methodSymbol.owner.type;
       }
       if (enclosingType != null) {
-        invokedMethodType = state.getTypes().memberType(enclosingType, methodSymbol);
+        invokedMethodType =
+            TypeSubstitutionUtils.memberType(state.getTypes(), enclosingType, methodSymbol);
       }
     }
     // substitute type arguments for generic methods
@@ -694,7 +696,8 @@ public final class GenericsChecks {
     // Obtain type parameters for the overridden method within the context of the overriding
     // method's class
     Type methodWithTypeParams =
-        state.getTypes().memberType(overridingMethod.owner.type, overriddenMethod);
+        TypeSubstitutionUtils.memberType(
+            state.getTypes(), overridingMethod.owner.type, overriddenMethod);
 
     checkTypeParameterNullnessForOverridingMethodReturnType(
         tree, methodWithTypeParams, analysis, state);
@@ -772,7 +775,8 @@ public final class GenericsChecks {
       // annotation should have been handled by the caller)
       return Nullness.NONNULL;
     }
-    Type overriddenMethodType = state.getTypes().memberType(enclosingType, method);
+    Type overriddenMethodType =
+        TypeSubstitutionUtils.memberType(state.getTypes(), enclosingType, method);
     verify(
         overriddenMethodType instanceof ExecutableType,
         "expected ExecutableType but instead got %s",
@@ -873,7 +877,9 @@ public final class GenericsChecks {
 
     Type.ForAll forAllType = (Type.ForAll) methodSymbol.type;
     Type.MethodType underlyingMethodType = (Type.MethodType) forAllType.qtype;
-    return state.getTypes().subst(underlyingMethodType, forAllType.tvars, explicitTypeArgs);
+    Types types = state.getTypes();
+    return TypeSubstitutionUtils.subst(
+        types, underlyingMethodType, forAllType.tvars, explicitTypeArgs);
   }
 
   /**
@@ -1007,7 +1013,7 @@ public final class GenericsChecks {
       // @Nullable annotation is handled elsewhere)
       return Nullness.NONNULL;
     }
-    Type methodType = state.getTypes().memberType(enclosingType, method);
+    Type methodType = TypeSubstitutionUtils.memberType(state.getTypes(), enclosingType, method);
     Type paramType = methodType.getParameterTypes().get(parameterIndex);
     return getTypeNullness(paramType, config);
   }
