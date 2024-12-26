b1fe3f7a70def0d19882b773a8a3af433aa26461
Manu Sridharan <msridhar@gmail.com>
2024-12-25 20:01:17 -0800

    WIP

--- nullaway/src/main/java/com/uber/nullaway/generics/TypeSubstitutionUtils.java
+++ nullaway/src/main/java/com/uber/nullaway/generics/TypeSubstitutionUtils.java
@@ -17,7 +17,46 @@ public class TypeSubstitutionUtils {
    * @return the type of {@code sym} as a member of {@code t}
    */
   public static Type memberType(Types types, Type t, Symbol sym) {
-    return types.memberType(t, sym);
+    Type origType = sym.type;
+    Type memberType = types.memberType(t, sym);
+    return restoreExplicitNullabilityAnnotations(origType, memberType);
+  }
+
+  private static Type restoreExplicitNullabilityAnnotations(Type origType, Type newType) {
+    // we want to visit the types together; they should have the same structure, except that some
+    // stuff from origType got substituted out.  if at any point we encounter an explicit @Nullable
+    // or @NonNull annotation on origType, restore it to the corresponding substituted type in
+    // newType.  NOTE: we cannot just tweak the substitution, since explicit annotations may appear
+    // only at some occurrences of a type.  TEST THIS.
+    return new RestoreNullnessAnnotationsVisitor().visit(newType, origType);
+  }
+
+  // NOTE: com.sun.tools.javac.code.Type.StructuralTypeMapping is really good to look at; need to
+  // mimick that structure
+  private static class RestoreNullnessAnnotationsVisitor extends Types.MapVisitor<Type> {
+
+    @Override
+    public Type visitMethodType(Type.MethodType t, Type other) {
+      Type.MethodType otherMethodType = (Type.MethodType) other;
+      List<Type> argtypes = t.argtypes;
+      Type restype = t.restype;
+      List<Type> thrown = t.thrown;
+      List<Type> argtypes1 = visit(argtypes, otherMethodType.argtypes);
+      Type restype1 = visit(restype, otherMethodType.restype);
+      List<Type> thrown1 = visit(thrown, otherMethodType.thrown);
+      if (argtypes1 == argtypes && restype1 == restype && thrown1 == thrown) return t;
+      else
+        return new Type.MethodType(argtypes1, restype1, thrown1, t.tsym) {
+          @Override
+          protected boolean needsStripping() {
+            return true;
+          }
+        };
+    }
+
+    private List<Type> visit(List<Type> argtypes, List<Type> argtypes1) {
+      return null;
+    }
   }
 
   /**
