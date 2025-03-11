c2743daf278515470020ccec2a59485665db131a
Manu Sridharan <msridhar@gmail.com>
2025-03-10 18:01:47 -0700

    whoops

--- nullaway/src/main/java/com/uber/nullaway/dataflow/AccessPathNullnessPropagation.java
+++ nullaway/src/main/java/com/uber/nullaway/dataflow/AccessPathNullnessPropagation.java
@@ -1089,9 +1089,8 @@ public class AccessPathNullnessPropagation
       MethodInvocationTree tree = node.getTree();
       if (tree != null) {
         Nullness nullness =
-            new GenericsChecks()
-                .getGenericReturnNullnessAtInvocation(
-                    ASTHelpers.getSymbol(tree), tree, state, config);
+            genericsChecks.getGenericReturnNullnessAtInvocation(
+                ASTHelpers.getSymbol(tree), tree, state, config);
         return nullness.equals(NULLABLE);
       }
     }
