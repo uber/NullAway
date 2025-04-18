23e2e39999cf171eeb0d1ed1784a812b1021c268
Manu Sridharan <msridhar@gmail.com>
2025-04-18 09:21:41 -0700

    JSpecify: handle varargs whose element type is a type variable (#1200)
    Partially addresses #1199. After substituting for a type variable, we
    were not retrieving the correct nullability for a varargs parameter,
    which should be the nullability of the component, not the array type
    itself.
    
    Also adds some simplification of our handling of varargs calls based on
    a tip from @jeffrey-easyesi

--- nullaway/src/test/java/com/uber/nullaway/jspecify/GenericMethodTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/GenericMethodTests.java
@@ -526,6 +526,78 @@ public class GenericMethodTests extends NullAwayTestsBase {
         .doTest();
   }
 
+  @Test
+  public void varargsOfGenericType() {
+    makeHelper()
+        .addSourceLines(
+            "Varargs.java",
+            "import org.jspecify.annotations.NullMarked;",
+            "import org.jspecify.annotations.Nullable;",
+            "@NullMarked",
+            "public class Varargs {",
+            "    static <T extends @Nullable Object> void foo(T... args) {",
+            "    }",
+            "    static void testNegative(@Nullable String s) {",
+            "        Varargs.<@Nullable String>foo(s);",
+            "    }",
+            "    static void testPositive(@Nullable String s) {",
+            "        // BUG: Diagnostic contains: passing @Nullable parameter",
+            "        Varargs.<String>foo(s);",
+            "    }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void nullableVarargsArray() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            "import org.jspecify.annotations.Nullable;",
+            "import org.jspecify.annotations.NullMarked;",
+            "@NullMarked",
+            "class Test {",
+            "    <T extends Object> void varargsTest(T @Nullable... args) {}",
+            "    void f() {",
+            "        String[] x = null;",
+            "        this.<String>varargsTest(x);",
+            "        this.<String>varargsTest((String[])null);",
+            "    }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void varargsConstructor() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            "import org.jspecify.annotations.Nullable;",
+            "import org.jspecify.annotations.NullMarked;",
+            "@NullMarked",
+            "class Test {",
+            "    static class Foo {",
+            "      <T> Foo(T @Nullable... args) {}",
+            "    }",
+            "    void testNegative() {",
+            "        String[] x = null;",
+            "        Foo f = new <String>Foo(x);",
+            "        f = new <String>Foo((String[])null);",
+            "    }",
+            "    static class Bar {",
+            "      <T> Bar(T... args) {}",
+            "    }",
+            "    void testPositive() {",
+            "        String[] x = null;",
+            "        // BUG: Diagnostic contains: passing @Nullable parameter",
+            "        Bar b = new <String>Bar(x);",
+            "        // BUG: Diagnostic contains: passing @Nullable parameter",
+            "        b = new <String>Bar((String[])null);",
+            "    }",
+            "}")
+        .doTest();
+  }
+
   private CompilationTestHelper makeHelper() {
     return makeTestHelperWithArgs(
         Arrays.asList(
