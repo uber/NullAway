ca4c7f6fc3fc03956401904cdf4654d2e0deb8d2
Rory MacQueen <rorymacqueen@gmail.com>
2025-08-09 14:32:53 -0400

    New additions based on AI suggestions

--- nullaway/src/main/java/com/uber/nullaway/handlers/LibraryModelsHandler.java
+++ nullaway/src/main/java/com/uber/nullaway/handlers/LibraryModelsHandler.java
@@ -725,9 +725,9 @@ public class LibraryModelsHandler extends BaseNoOpHandler {
                 0)
             .put(
                 methodRef(
-                    "com.amazonaws.util.CollectionUtils",
-                    "isNullOrEmpty(java.util.Collection<?>)"),
+                    "com.amazonaws.util.CollectionUtils", "isNullOrEmpty(java.util.Collection<?>)"),
                 0)
+            .put(methodRef("com.amazonaws.util.StringUtils", "isNullOrEmpty(java.lang.String)"),0)
             .put(methodRef("org.springframework.util.StringUtils", "isEmpty(java.lang.Object)"), 0)
             .put(methodRef("org.springframework.util.ObjectUtils", "isEmpty(java.lang.Object)"), 0)
             .put(methodRef("spark.utils.ObjectUtils", "isEmpty(java.lang.Object[])"), 0)
