8b9fef8dc394c6447d34ceecfae46a5cce614529
Yeahn Kim <yeahnkim@gmail.com>
2025-08-21 16:52:59 -0700

    test-unannotated

---
+++ jdk-annotations/test-unannotated/src/main/java/com/test/ParameterAnnotated.java
@@ -0,0 +1,41 @@
+package com.test;
+
+public class ParameterAnnotated {
+
+  public static Integer add(Integer a, Integer b) {
+    return a + b;
+  }
+
+  public static Object getNewObjectIfNull(Object object) {
+    if (object == null) {
+      return new Object();
+    } else {
+      return object;
+    }
+  }
+
+  public static void printObjectString(Object object) {
+    System.out.println(object.toString());
+  }
+
+  @SuppressWarnings("ArrayToString")
+  public static void takesNullArray(Object[] objects) {
+    System.out.println(objects);
+  }
+
+  @SuppressWarnings("ArrayToString")
+  public static void takesNonNullArray(Object[] objects) {
+    String unused = objects.toString();
+  }
+
+  public static class Generic<T> {
+
+    public String getString(T t) {
+      return t != null ? t.toString() : "";
+    }
+
+    public void printObjectString(T t) {
+      System.out.println(t.toString());
+    }
+  }
+}
---
+++ jdk-annotations/test-unannotated/src/main/java/com/test/ReturnAnnotated.java
@@ -0,0 +1,68 @@
+package com.test;
+
+import java.util.Locale;
+
+public class ReturnAnnotated {
+
+  // return type : null, nullable, nonnull && type parameter, predefined types
+  // type parameter: nada, exists && nullable, nonnull
+
+  public String nullReturn() {
+    return null;
+  }
+
+  public String nonNullString() {
+    return new String();
+  }
+
+  public <T> T getTypePar(T t) {
+    return t;
+  }
+
+  public String makeUpperCase(String inputString) {
+    if (inputString == null || inputString.isEmpty()) {
+      return null;
+    } else {
+      return inputString.toUpperCase(Locale.ROOT);
+    }
+  }
+
+  public Integer[] generateIntArray(int size) {
+    if (size <= 0) {
+      return null;
+    } else {
+      Integer[] result = new Integer[size];
+      for (int i = 0; i < size; i++) {
+        result[i] = i + 1;
+      }
+      return result;
+    }
+  }
+
+  public static class InnerExample {
+    public String returnNull() {
+      return null;
+    }
+  }
+
+  public static class UpperBoundExample<T> {
+
+    T nullableObject;
+
+    public T getNullable() {
+      return nullableObject;
+    }
+  }
+
+  public static Integer add(Integer a, Integer b) {
+    return a + b;
+  }
+
+  public static Object getNewObjectIfNull(Object object) {
+    if (object == null) {
+      return new Object();
+    } else {
+      return object;
+    }
+  }
+}
