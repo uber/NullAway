6dbc9f86edd03271d92255eb40b6ab08b70d3d7c
Manu Sridharan <msridhar@gmail.com>
2025-09-11 15:42:48 -0700

    more

--- nullaway/src/main/java/com/uber/nullaway/InvocationArguments.java
+++ nullaway/src/main/java/com/uber/nullaway/InvocationArguments.java
@@ -10,12 +10,10 @@ import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Type;
-import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import org.jspecify.annotations.Nullable;
 
-public class InvocationArguments implements Iterable<InvocationArguments.ArgumentInfo> {
+public class InvocationArguments {
 
   // Cached args as array to avoid O(n) indexed access on javac lists
   private final ExpressionTree[] argsArr;
@@ -44,11 +42,12 @@ public class InvocationArguments implements Iterable<InvocationArguments.Argumen
     } else {
       throw new IllegalStateException("Unexpected invocation tree type");
     }
-    this.argsArr = toArgArray(argsList);
+    this.argsArr = argsList.toArray(new ExpressionTree[argsList.size()]);
     this.numArgsPassed = argsArr.length;
 
     // Cache parameter types as array (fast indexed access)
-    this.paramTypesArr = toParamArray(invokedMethodType.getParameterTypes());
+    com.sun.tools.javac.util.List<Type> parameterTypes = invokedMethodType.getParameterTypes();
+    this.paramTypesArr = parameterTypes.toArray(new Type[parameterTypes.size()]);
 
     // Precompute varargs state and related types
     this.isVarArgs = methodSymbol.isVarArgs();
@@ -65,7 +64,6 @@ public class InvocationArguments implements Iterable<InvocationArguments.Argumen
     }
   }
 
-  // Fast, zero-allocation traversal (no ArgumentInfo objects)
   public void forEachFast(ArgConsumer consumer) {
     if (!isVarArgs) {
       for (int i = 0; i < numArgsPassed; i++) {
@@ -93,161 +91,9 @@ public class InvocationArguments implements Iterable<InvocationArguments.Argumen
     }
   }
 
-  public int size() {
-    return numArgsPassed;
-  }
-
-  @Override
-  public Iterator<ArgumentInfo> iterator() {
-    if (!isVarArgs) {
-      return new RegularArgIterator(argsArr, paramTypesArr);
-    }
-    return new VarArgsIterator(
-        argsArr,
-        paramTypesArr,
-        varArgsIndex,
-        varArgsPassedIndividually,
-        castToNonNull(varArgsArrayType),
-        castToNonNull(varArgsComponentType));
-  }
-
-  public static final class ArgumentInfo {
-
-    /** The argument expression tree. */
-    private final ExpressionTree argTree;
-
-    private final int argPos;
-
-    /** The formal parameter type corresponding to this argument. */
-    private final Type formalParamType;
-
-    /**
-     * Whether this argument is an array being passed in the varargs position (i.e., it contains all
-     * the varargs arguments).
-     */
-    private final boolean varArgsPassedAsArray;
-
-    ArgumentInfo(
-        ExpressionTree argTree, int argPos, Type formalParamType, boolean varArgsPassedAsArray) {
-      this.argTree = argTree;
-      this.argPos = argPos;
-      this.formalParamType = formalParamType;
-      this.varArgsPassedAsArray = varArgsPassedAsArray;
-    }
-
-    public ExpressionTree getArgTree() {
-      return argTree;
-    }
-
-    public int getArgPos() {
-      return argPos;
-    }
-
-    public Type getFormalParamType() {
-      return formalParamType;
-    }
-
-    public boolean isVarArgsPassedAsArray() {
-      return varArgsPassedAsArray;
-    }
-  }
-
   @FunctionalInterface
   public interface ArgConsumer {
     void accept(
         ExpressionTree argTree, int argPos, Type formalParamType, boolean varArgsPassedAsArray);
-  }
-
-  // Regular (non-varargs) iterator: minimal branching, fast indexed access
-  private static final class RegularArgIterator implements Iterator<ArgumentInfo> {
-    private final ExpressionTree[] args;
-    private final Type[] paramTypes;
-    private int idx = 0;
-
-    RegularArgIterator(ExpressionTree[] args, Type[] paramTypes) {
-      this.args = args;
-      this.paramTypes = paramTypes;
-    }
-
-    @Override
-    public boolean hasNext() {
-      return idx < args.length;
-    }
-
-    @Override
-    public ArgumentInfo next() {
-      if (!hasNext()) {
-        throw new NoSuchElementException();
-      }
-      int i = idx++;
-      return new ArgumentInfo(args[i], i, paramTypes[i], false);
-    }
-  }
-
-  // Varargs iterator: uses precomputed array/component types, avoids per-next lookups/casts
-  private static final class VarArgsIterator implements Iterator<ArgumentInfo> {
-    private final ExpressionTree[] args;
-    private final Type[] paramTypes;
-    private final int varArgsIndex;
-    private final boolean passedIndividually;
-    private final Type.ArrayType varArgsArrayType;
-    private final Type varArgsComponentType;
-    private int idx = 0;
-
-    VarArgsIterator(
-        ExpressionTree[] args,
-        Type[] paramTypes,
-        int varArgsIndex,
-        boolean passedIndividually,
-        Type.ArrayType varArgsArrayType,
-        Type varArgsComponentType) {
-      this.args = args;
-      this.paramTypes = paramTypes;
-      this.varArgsIndex = varArgsIndex;
-      this.passedIndividually = passedIndividually;
-      this.varArgsArrayType = varArgsArrayType;
-      this.varArgsComponentType = varArgsComponentType;
-    }
-
-    @Override
-    public boolean hasNext() {
-      return idx < args.length;
-    }
-
-    @Override
-    public ArgumentInfo next() {
-      if (!hasNext()) {
-        throw new NoSuchElementException();
-      }
-      ExpressionTree argTree = args[idx];
-      ArgumentInfo info;
-      if (idx < varArgsIndex) {
-        info = new ArgumentInfo(argTree, idx, paramTypes[idx], false);
-      } else if (passedIndividually) {
-        info = new ArgumentInfo(argTree, idx, varArgsComponentType, false);
-      } else {
-        info = new ArgumentInfo(argTree, idx, varArgsArrayType, true);
-      }
-      idx++;
-      return info;
-    }
-  }
-
-  private static ExpressionTree[] toArgArray(List<? extends ExpressionTree> list) {
-    ExpressionTree[] arr = new ExpressionTree[list.size()];
-    int i = 0;
-    for (ExpressionTree e : list) {
-      arr[i++] = e;
-    }
-    return arr;
-  }
-
-  private static Type[] toParamArray(com.sun.tools.javac.util.List<Type> list) {
-    Type[] arr = new Type[list.size()];
-    int i = 0;
-    for (Type t : list) {
-      arr[i++] = t;
-    }
-    return arr;
   }
 }
