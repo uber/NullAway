d487afcfb23ced6644c58b9dd15b68e481d5d9ca
Manu Sridharan <msridhar@gmail.com>
2025-09-11 16:02:37 -0700

    another test

--- nullaway/src/test/java/com/uber/nullaway/jspecify/GenericMethodTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/GenericMethodTests.java
@@ -830,6 +830,30 @@ public class GenericMethodTests extends NullAwayTestsBase {
         .doTest();
   }
 
+  @Test
+  public void varargsInferencePassingArray() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            "import org.jspecify.annotations.*;",
+            "@NullMarked",
+            "class Test {",
+            "    static class Foo<T extends @Nullable Object> {}",
+            "    public static <U extends @Nullable Object> Foo<U> make(U... args) {",
+            "      throw new RuntimeException();",
+            "    }",
+            "    static <V extends @Nullable String> V makeStr(V v) {",
+            "      return v;",
+            "    }",
+            "    Foo<String> foo1 = make(new String[] { \"hello\", \"world\" });",
+            "    Foo<@Nullable String> foo2 = make(new @Nullable String[] { \"hello\", null, \"world\" });",
+            "    // BUG: Diagnostic contains: incompatible types",
+            "    Foo<String> foo3 = make(new @Nullable String[] { \"hello\", null, \"world\" });",
+            "    Foo<@Nullable String> foo4 = make(new String[] { \"hello\", \"world\" });",
+            "}")
+        .doTest();
+  }
+
   @Ignore("need better handling of lambdas")
   @Test
   public void supplierLambdaInference() {
