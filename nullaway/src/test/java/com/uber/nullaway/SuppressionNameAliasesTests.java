46943a23a4b32463cbb2d68c28a487019f266894
Lazaro Clapp <lazaro.clapp@gmail.com>
2025-10-08 12:56:53 -0400

    [WIP] Generic suppression alias for nullness

--- nullaway/src/test/java/com/uber/nullaway/SuppressionNameAliasesTests.java
+++ nullaway/src/test/java/com/uber/nullaway/SuppressionNameAliasesTests.java
@@ -6,6 +6,26 @@ import org.junit.Test;
 public class SuppressionNameAliasesTests extends NullAwayTestsBase {
 
   @Test
+  public void nullnessSuppressesNullAway() {
+    makeTestHelperWithArgs(
+            Arrays.asList(
+                "-d",
+                temporaryFolder.getRoot().getAbsolutePath(),
+                "-XepOpt:NullAway:AnnotatedPackages=com.uber"))
+        .addSourceLines(
+            "Test.java",
+            "package com.uber;",
+            "import org.jspecify.annotations.Nullable;",
+            "class Test {",
+            "  @SuppressWarnings(\"nullness\")",
+            "  void foo(@Nullable Object o) {",
+            "    o.getClass();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
   public void additionalSuppressionNamesTest() {
     makeTestHelperWithArgs(
             Arrays.asList(
