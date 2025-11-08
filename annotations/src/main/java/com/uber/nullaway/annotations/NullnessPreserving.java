1e2be64d8e4b54090d83bde086afc2de544a19ec
Manu Sridharan <msridhar@gmail.com>
2025-11-08 15:38:23 -0800

    fix

--- annotations/src/main/java/com/uber/nullaway/annotations/NullnessPreserving.java
+++ annotations/src/main/java/com/uber/nullaway/annotations/NullnessPreserving.java
@@ -1,6 +1,9 @@
 package com.uber.nullaway.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Indicates that a method or constructor preserves the nullness environment when invoking lambda
