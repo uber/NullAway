cdf0d816bee8587881fdabe9e4542ea2373a150d
Romain Moreau <romainmoreau@gmail.com>
2025-12-21 00:33:34 +0100

    Support added for JUnit's @TempDir

--- nullaway/src/main/java/com/uber/nullaway/ErrorProneCLIFlagsConfig.java
+++ nullaway/src/main/java/com/uber/nullaway/ErrorProneCLIFlagsConfig.java
@@ -183,7 +183,8 @@ final class ErrorProneCLIFlagsConfig implements Config {
           "org.springframework.boot.test.mock.mockito.MockBean",
           "org.springframework.boot.test.mock.mockito.SpyBean",
           "org.springframework.test.context.bean.override.mockito.MockitoBean",
-          "org.springframework.test.context.bean.override.mockito.MockitoSpyBean");
+          "org.springframework.test.context.bean.override.mockito.MockitoSpyBean",
+          "org.junit.jupiter.api.io.TempDir");
 
   private static final String DEFAULT_URL = "http://t.uber.com/nullaway";
 
