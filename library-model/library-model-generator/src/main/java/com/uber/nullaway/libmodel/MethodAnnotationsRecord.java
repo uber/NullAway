ed33a433937cc289fb724cbfb9d2d18fd6d559b6
rishik <rishikraj990@gmail.com>
2025-12-29 14:54:12 +0530

    migrated MethodAnnotationsRecord on record

--- library-model/library-model-generator/src/main/java/com/uber/nullaway/libmodel/MethodAnnotationsRecord.java
+++ library-model/library-model-generator/src/main/java/com/uber/nullaway/libmodel/MethodAnnotationsRecord.java
@@ -1,24 +1,22 @@
 package com.uber.nullaway.libmodel;
 
-import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
-/** A record describing the annotations associated with a java method and its arguments. */
-@AutoValue
-public abstract class MethodAnnotationsRecord {
+/** A record describing the annotations associated with a java method and its arguments.
+ * Generates by Record
+ */
+
+public record MethodAnnotationsRecord(ImmutableSet<String> methodAnnotations,
+                                      ImmutableSet<Integer> typeParamNullableUpperbounds,
+                                      ImmutableMap<Integer, ImmutableSet<String>> argumentAnnotations) {
 
   public static MethodAnnotationsRecord create(
       ImmutableSet<String> methodAnnotations,
       ImmutableSet<Integer> typeParamNullableUpperbounds,
       ImmutableMap<Integer, ImmutableSet<String>> argumentAnnotations) {
-    return new AutoValue_MethodAnnotationsRecord(
+    return new MethodAnnotationsRecord(
         methodAnnotations, typeParamNullableUpperbounds, argumentAnnotations);
   }
 
-  abstract ImmutableSet<String> methodAnnotations();
-
-  abstract ImmutableSet<Integer> typeParamNullableUpperbounds();
-
-  abstract ImmutableMap<Integer, ImmutableSet<String>> argumentAnnotations();
 }
