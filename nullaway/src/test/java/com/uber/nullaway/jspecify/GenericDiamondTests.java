22fb655208a575d7cadbfabbed872e4ec2340e31
Manu Sridharan <msridhar@gmail.com>
2026-02-05 17:27:11 -0800

    test case

--- nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
@@ -4,8 +4,35 @@ import com.google.errorprone.CompilationTestHelper;
 import com.uber.nullaway.NullAwayTestsBase;
 import com.uber.nullaway.generics.JSpecifyJavacConfig;
 import java.util.Arrays;
+import org.junit.Test;
 
 public class GenericDiamondTests extends NullAwayTestsBase {
+
+  @Test
+  public void issue1451() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            """
+            import org.jspecify.annotations.*;
+            @NullMarked
+            public class Test {
+              static class Foo<T extends @Nullable Object> {
+                static Foo<@Nullable Void> make() {
+                  throw new RuntimeException();
+                }
+              }
+              static class Bar<T extends @Nullable Object> {
+                Bar(Foo<T> foo) {
+                }
+              }
+              void test() {
+                Bar<@Nullable Void> b = new Bar<>(Foo.make());
+              }
+            }
+            """)
+        .doTest();
+  }
 
   private CompilationTestHelper makeHelper() {
     return makeTestHelperWithArgs(
