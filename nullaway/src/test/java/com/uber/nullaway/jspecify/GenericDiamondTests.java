e86b344a7f418a822a896fc742cf9e2e17f706f9
Manu Sridharan <msridhar@gmail.com>
2026-02-05 17:31:07 -0800

    tweak test

--- nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
@@ -21,13 +21,21 @@ public class GenericDiamondTests extends NullAwayTestsBase {
                 static Foo<@Nullable Void> make() {
                   throw new RuntimeException();
                 }
+                static Foo<@Nullable String> makeNullableStr() {
+                  throw new RuntimeException();
+                }
               }
               static class Bar<T extends @Nullable Object> {
                 Bar(Foo<T> foo) {
                 }
               }
-              void test() {
+              void testNegative() {
+                // should be legal
                 Bar<@Nullable Void> b = new Bar<>(Foo.make());
+              }
+              void testPositive() {
+                // BUG: Diagnostic contains:
+                Bar<String> b = new Bar<>(Foo.makeNullableStr());
               }
             }
             """)
