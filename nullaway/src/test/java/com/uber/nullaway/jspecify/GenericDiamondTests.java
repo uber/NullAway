81f952c6f75b000fa6a75263aeca0131d09ce4a7
Manu Sridharan <msridhar@gmail.com>
2026-02-05 22:46:59 -0800

    better tests

--- nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/GenericDiamondTests.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class GenericDiamondTests extends NullAwayTestsBase {
 
   @Test
-  public void issue1451() {
+  public void assignToLocal() {
     makeHelper()
         .addSourceLines(
             "Test.java",
@@ -36,6 +36,76 @@ public class GenericDiamondTests extends NullAwayTestsBase {
               void testPositive() {
                 // BUG: Diagnostic contains:
                 Bar<String> b = new Bar<>(Foo.makeNullableStr());
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
+  public void returnDiamond() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            """
+            import org.jspecify.annotations.*;
+            @NullMarked
+            public class Test {
+              static class Foo<T extends @Nullable Object> {
+                static Foo<@Nullable Void> make() {
+                  throw new RuntimeException();
+                }
+                static Foo<@Nullable String> makeNullableStr() {
+                  throw new RuntimeException();
+                }
+              }
+              static class Bar<T extends @Nullable Object> {
+                Bar(Foo<T> foo) {
+                }
+              }
+              Bar<@Nullable Void> testNegative() {
+                // should be legal
+                return new Bar<>(Foo.make());
+              }
+              Bar<String> testPositive() {
+                // BUG: Diagnostic contains:
+                return new Bar<>(Foo.makeNullableStr());
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
+  public void paramPassing() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            """
+            import org.jspecify.annotations.*;
+            @NullMarked
+            public class Test {
+              static class Foo<T extends @Nullable Object> {
+                static Foo<@Nullable Void> make() {
+                  throw new RuntimeException();
+                }
+                static Foo<@Nullable String> makeNullableStr() {
+                  throw new RuntimeException();
+                }
+              }
+              static class Bar<T extends @Nullable Object> {
+                Bar(Foo<T> foo) {
+                }
+              }
+              static void takeNullableVoid(Bar<@Nullable Void> b) {}
+              static void takeStr(Bar<String> b) {}
+              void testNegative() {
+                // should be legal
+                takeNullableVoid(new Bar<>(Foo.make()));
+              }
+              void testPositive() {
+                // BUG: Diagnostic contains:
+                takeStr(new Bar<>(Foo.makeNullableStr()));
               }
             }
             """)
