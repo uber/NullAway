5f46f67030c8523b074136a01e1fdde529ddc229
Yeahn Kim <yeahnkim@gmail.com>
2026-02-14 20:25:33 -0800

    test for new option

---
+++ nullaway/src/test/java/com/uber/nullaway/LibraryModelsHandlerTest.java
@@ -0,0 +1,51 @@
+package com.uber.nullaway;
+
+import com.google.errorprone.CompilationTestHelper;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LibraryModelsHandlerTest extends NullAwayTestsBase {
+
+  @Test
+  public void jdkInferEnabledLoadsAstubxModel() {
+    CompilationTestHelper compilationTestHelper =
+        makeTestHelperWithArgs(
+                List.of(
+                    "-XepOpt:NullAway:AnnotatedPackages=foo",
+                    "-XepOpt:NullAway:JDKInferEnabled=true"))
+            .addSourceLines(
+                "Test.java",
+                "package foo;",
+                "import javax.naming.directory.Attributes;",
+                "import org.jspecify.annotations.NullMarked;",
+                "@NullMarked",
+                "class Test {",
+                "  void use(Attributes attrs) {",
+                "    // BUG: Diagnostic contains: @Nullable",
+                "    attrs.get(\"key\").toString();",
+                "  }",
+                "}");
+    compilationTestHelper.doTest();
+  }
+
+  @Test
+  public void jdkInferDisabledDoesNotLoadAstubxModel() {
+    CompilationTestHelper compilationTestHelper =
+        makeTestHelperWithArgs(List.of("-XepOpt:NullAway:AnnotatedPackages=foo"))
+            .addSourceLines(
+                "Test.java",
+                "package foo;",
+                "import javax.naming.directory.Attributes;",
+                "import org.jspecify.annotations.NullMarked;",
+                "@NullMarked",
+                "class Test {",
+                "  void use(Attributes attrs) {",
+                "    attrs.get(\"key\").toString();",
+                "  }",
+                "}");
+    compilationTestHelper.doTest();
+  }
+}
