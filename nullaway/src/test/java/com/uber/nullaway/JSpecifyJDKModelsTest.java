d464a5b9c13a54fac47838d905cd7afdf97bbab5
Manu Sridharan <msridhar@gmail.com>
2026-02-20 18:42:00 -0800

    more tests

--- nullaway/src/test/java/com/uber/nullaway/JSpecifyJDKModelsTest.java
+++ nullaway/src/test/java/com/uber/nullaway/JSpecifyJDKModelsTest.java
@@ -69,9 +69,43 @@ public class JSpecifyJDKModelsTest extends NullAwayTestsBase {
             import org.jspecify.annotations.Nullable;
             @NullMarked
             class Test {
+              void testNullableContents(List<@Nullable String> list) {
+                list.add(null);
+                // BUG: Diagnostic contains: dereferenced expression list.get(0) is @Nullable
+                list.get(0).toString();
+              }
+              void testNonNullContents(List<String> list) {
+                // BUG: Diagnostic contains: passing @Nullable parameter 'null' where @NonNull is required
+                list.add(null);
+                list.get(0).toString();
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
+  public void listContainingNullsWithoutModel() {
+    makeTestHelperWithArgs(
+            JSpecifyJavacConfig.withJSpecifyModeArgs(
+                List.of("-XepOpt:NullAway:AnnotatedPackages=foo")))
+        .addSourceLines(
+            "Test.java",
+            """
+            package foo;
+            import java.util.List;
+            import org.jspecify.annotations.NullMarked;
+            import org.jspecify.annotations.Nullable;
+            @NullMarked
+            class Test {
               void use(List<@Nullable String> list) {
-                // list.add(null);
-                // BUG: Diagnostic contains:
+                list.add(null);
+                // no warning, since List.get() is unmarked without the model
+                list.get(0).toString();
+              }
+              void testNonNullContents(List<String> list) {
+                // no warning, since List.add() is unmarked without the model
+                list.add(null);
                 list.get(0).toString();
               }
             }
