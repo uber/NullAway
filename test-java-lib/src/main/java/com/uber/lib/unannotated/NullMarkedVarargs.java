579276965199505e0ca5bb34d3ca968d59c0179e
Manu Sridharan <msridhar@gmail.com>
2026-02-24 14:25:32 -0800

    Add NullMarkedVarargs.java

---
+++ test-java-lib/src/main/java/com/uber/lib/unannotated/NullMarkedVarargs.java
@@ -0,0 +1,11 @@
+package com.uber.lib.unannotated;
+
+/* @NullMarked */
+public class NullMarkedVarargs {
+
+  public static void nullableContents(/* @Nullable */ String... args) {}
+
+  public static void nullableArray(String /* @Nullable */[] args) {}
+
+  public static void bothNullable(/* @Nullable */ String /* @Nullable */... args) {}
+}
