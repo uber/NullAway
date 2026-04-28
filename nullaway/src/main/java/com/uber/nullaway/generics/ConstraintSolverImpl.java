36ffec3d82b991653744996c7be99d7d2355b823
Manu Sridharan <msridhar@gmail.com>
2026-04-28 12:56:25 -0700

    coderabbit

--- nullaway/src/main/java/com/uber/nullaway/generics/ConstraintSolverImpl.java
+++ nullaway/src/main/java/com/uber/nullaway/generics/ConstraintSolverImpl.java
@@ -164,8 +164,10 @@ public final class ConstraintSolverImpl implements ConstraintSolver {
       if (config.handleWildcardGenerics()) {
         Verify.verify(!localVariableType, "A wildcard type cannot be assigned to a local variable");
         constrainWildcardToSupertype(subtype, supertype);
+        return null;
+      } else {
+        return visitType(subtype, supertype);
       }
-      return null;
     }
 
     /**
