c7a52fd6817a198ce60ff3bbec142904c614b4c7
Manu Sridharan <msridhar@gmail.com>
2026-05-01 12:02:23 -0700

    add test for locals

--- nullaway/src/test/java/com/uber/nullaway/jspecify/WildcardTests.java
+++ nullaway/src/test/java/com/uber/nullaway/jspecify/WildcardTests.java
@@ -437,6 +437,43 @@ public class WildcardTests extends NullAwayTestsBase {
   }
 
   @Test
+  public void wildcardCaptureLocals() {
+    makeHelper()
+        .addSourceLines(
+            "Test.java",
+            """
+            import org.jspecify.annotations.NullMarked;
+            import org.jspecify.annotations.Nullable;
+            @NullMarked
+            class Test {
+              static class Foo<T extends @Nullable Object> {
+                T get() { throw new RuntimeException(); }
+              }
+              static void testNullableExtendsBound(Foo<? extends @Nullable Object> f) {
+                Object x = f.get();
+                // BUG: Diagnostic contains: dereferenced expression x is @Nullable
+                x.hashCode();
+              }
+              static void testNonNullExtendsBound(Foo<? extends Object> f) {
+                Object x = f.get();
+                // this is legal
+                x.hashCode();
+              }
+              static void testNullableSuperBound(Foo<? super @Nullable String> f) {
+                Object x = f.get();
+                // BUG: Diagnostic contains: dereferenced expression x is @Nullable
+                x.hashCode();
+              }
+              static void testNonNullSuperBound(Foo<? super String> f) {
+                Object x = f.get();
+                // BUG: Diagnostic contains: dereferenced expression x is @Nullable
+                x.hashCode();
+              }
+            }""")
+        .doTest();
+  }
+
+  @Test
   public void wildcardSuperBoundsAndInference() {
     makeHelperWithInferenceFailureWarning()
         .addSourceLines(
