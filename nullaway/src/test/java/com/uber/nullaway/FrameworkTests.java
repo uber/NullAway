47dfbfa6064cf087dbcc4f9550fb58e5e7513671
subhramit <subhramit.bb@live.in>
2026-06-15 22:19:24 +0530

    Reformat
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- nullaway/src/test/java/com/uber/nullaway/FrameworkTests.java
+++ nullaway/src/test/java/com/uber/nullaway/FrameworkTests.java
@@ -1478,7 +1478,7 @@ public class FrameworkTests extends NullAwayTestsBase {
   @Test
   public void filesIsDirectory() {
     defaultCompilationHelper
-        .addSourceLines( // force changes
+        .addSourceLines(
             "Foo.java",
             """
             package com.uber;
