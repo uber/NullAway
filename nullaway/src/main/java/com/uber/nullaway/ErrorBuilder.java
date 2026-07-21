b906cc4fed707218e5b036891aab8b3ffbb8e8cb
Manu Sridharan <msridhar@gmail.com>
2026-07-20 17:24:42 -0700

    remove now unnecessary hacks

--- nullaway/src/main/java/com/uber/nullaway/ErrorBuilder.java
+++ nullaway/src/main/java/com/uber/nullaway/ErrorBuilder.java
@@ -32,7 +32,6 @@ import static com.uber.nullaway.NullAway.CORE_CHECK_NAME;
 import static com.uber.nullaway.NullAway.INITIALIZATION_CHECK_NAME;
 import static com.uber.nullaway.NullAway.OPTIONAL_CHECK_NAME;
 import static com.uber.nullaway.NullAway.getTreesInstance;
-import static com.uber.nullaway.NullabilityUtil.castToNonNull;
 import static com.uber.nullaway.Nullness.hasNullableAnnotation;
 
 import com.google.common.base.Joiner;
@@ -178,15 +177,14 @@ public class ErrorBuilder {
    * @return Whether the subchecker is being suppressed at treePath.
    */
   private boolean hasPathSuppression(TreePath treePath, String subcheckerName) {
-    // TODO inference and filter handling should handle this better
     return StreamSupport.stream(treePath.spliterator(), false)
         .filter(ErrorBuilder::canHaveSuppressWarningsAnnotation)
-        .<@Nullable Symbol>map(tree -> ASTHelpers.getSymbol(tree))
+        .map(ASTHelpers::getSymbol)
         .filter(Objects::nonNull)
         .anyMatch(
             symbol ->
-                symbolHasSuppressWarningsAnnotation(castToNonNull(symbol), subcheckerName)
-                    || symbolIsExcludedClassSymbol(castToNonNull(symbol)));
+                symbolHasSuppressWarningsAnnotation(symbol, subcheckerName)
+                    || symbolIsExcludedClassSymbol(symbol));
   }
 
   private Description.Builder addSuggestedSuppression(
