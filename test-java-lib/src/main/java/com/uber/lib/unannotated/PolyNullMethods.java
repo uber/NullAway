4ef47ff8da9df36ddc7ed65298ae67264102b09f
Manu Sridharan <msridhar@gmail.com>
2026-09-04 10:25:33 -0700

    improve

--- test-java-lib/src/main/java/com/uber/lib/unannotated/PolyNullMethods.java
+++ test-java-lib/src/main/java/com/uber/lib/unannotated/PolyNullMethods.java
@@ -9,26 +9,29 @@ public final class PolyNullMethods {
   private PolyNullMethods() {}
 
   /** Returns the first element available from either list. */
-  public static Object first(List<Object> first, List<Object> second) {
+  public static /* @PolyNull */ Object first(
+      List</* @PolyNull */ Object> first, List</* @PolyNull */ Object> second) {
     return first.isEmpty() ? second.get(0) : first.get(0);
   }
 
   /** Accepts two independently typed arguments. */
-  public static <T, U> void twoTypeVariables(T first, U second) {}
+  public static <T, U> void twoTypeVariables(/* @PolyNull */ T first, /* @PolyNull */ U second) {}
 
   /** Returns the first of two independently typed arguments. */
-  public static <T, U> T genericFirst(T first, U second) {
+  public static <T, U> /* @PolyNull */ T genericFirst(
+      /* @PolyNull */ T first, /* @PolyNull */ U second) {
     return first;
   }
 
   /** Returns the first argument as an object. */
-  public static <T, U> Object genericObject(T first, U second) {
+  public static <T, U> /* @PolyNull */ Object genericObject(
+      /* @PolyNull */ T first, /* @PolyNull */ U second) {
     return first;
   }
 
   /** Returns a value from the first of two independently typed suppliers. */
-  public static <T, U> T genericFromSuppliers(
-      Supplier<? extends T> first, Supplier<? extends U> second) {
+  public static <T, U> /* @PolyNull */ T genericFromSuppliers(
+      Supplier<? extends /* @PolyNull */ T> first, Supplier<? extends /* @PolyNull */ U> second) {
     return first.get();
   }
 }
