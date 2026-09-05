1e6bc8f28cafc2219d6dfc278c128408986df4ed
Manu Sridharan <msridhar@gmail.com>
2026-09-04 22:28:48 -0700

    cleanup

--- nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
+++ nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
@@ -1599,12 +1599,7 @@ public final class GenericsChecks {
       solver.addSubtypeConstraint(callResultType, typeFromAssignmentContext, assignedToLocal);
       if (polyNullContext != null) {
         PolyNullInference.addResultConstraints(
-            solver,
-            methodType.getReturnType(),
-            polyNullLocations,
-            polyNullContext,
-            typeFromAssignmentContext,
-            assignedToLocal);
+            solver, polyNullContext, typeFromAssignmentContext, assignedToLocal);
       }
     }
     // then, handle parameters
@@ -1612,7 +1607,7 @@ public final class GenericsChecks {
         state, path, solver, callTree, methodType, allCalls, polyNullContexts, calledFromDataflow);
     if (callTree instanceof MethodInvocationTree invocationTree
         && polyNullContext != null
-        && !polyNullContext.inputs().isEmpty()) {
+        && polyNullContext.hasInputLocations()) {
       generateArgumentConstraintsForCall(
           state,
           path,
@@ -3516,21 +3511,14 @@ public final class GenericsChecks {
             locations,
             getSyntheticNullableAnnotType(state),
             state);
-    if (inferenceContext.inputs().isEmpty()) {
+    if (!inferenceContext.hasInputLocations()) {
       return null;
     }
     ConstraintSolver solver = makeSolver(state, analysis);
     Set<Tree> nestedCalls = new LinkedHashSet<>();
     try {
       addPolyNullResultConstraintsFromDirectAssignmentContext(
-          invocationTree,
-          substitutedMethodType,
-          locations,
-          inferenceContext,
-          path,
-          solver,
-          state,
-          calledFromDataflow);
+          invocationTree, inferenceContext, path, solver, state, calledFromDataflow);
       generateArgumentConstraintsForCall(
           state,
           path,
@@ -3558,8 +3546,6 @@ public final class GenericsChecks {
    */
   private void addPolyNullResultConstraintsFromDirectAssignmentContext(
       MethodInvocationTree invocationTree,
-      Type.MethodType methodType,
-      ImmutableSet<PolyNullLocation> locations,
       PolyNullInferenceContext inferenceContext,
       @Nullable TreePath path,
       ConstraintSolver solver,
@@ -3592,8 +3578,6 @@ public final class GenericsChecks {
     if (callAndContext.typeFromAssignmentContext() != null) {
       PolyNullInference.addResultConstraints(
           solver,
-          methodType.getReturnType(),
-          locations,
           inferenceContext,
           callAndContext.typeFromAssignmentContext(),
           callAndContext.assignedToLocal());
