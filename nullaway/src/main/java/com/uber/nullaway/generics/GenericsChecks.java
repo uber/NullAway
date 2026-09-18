c44d002e616dde70b2c64dce2c9c6ee3d8b49468
Manu Sridharan <msridhar@gmail.com>
2026-09-17 18:24:56 -0700

    register inference variables

--- nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
+++ nullaway/src/main/java/com/uber/nullaway/generics/GenericsChecks.java
@@ -1570,6 +1570,7 @@ public final class GenericsChecks {
                         locations,
                         getSyntheticNullableAnnotType(state),
                         state));
+        solver.registerInferenceVariable(polyNullContext.inferenceVariable().asElement());
       }
     }
     // first, handle the call result flow
@@ -3490,6 +3491,7 @@ public final class GenericsChecks {
       return null;
     }
     ConstraintSolver solver = makeSolver(state, analysis);
+    solver.registerInferenceVariable(inferenceContext.inferenceVariable().asElement());
     Set<Tree> nestedCalls = new LinkedHashSet<>();
     try {
       addPolyNullResultConstraintsFromDirectAssignmentContext(
